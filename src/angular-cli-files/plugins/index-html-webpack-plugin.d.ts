fe0d333de5c662325da3f7d7f75fe024b7591ef7
Angular Builds <circleci@angular.io>
2018-04-30 19:34:53 +0000

    709fd48 refactor(@angular-devkit/build-angular): cleanup index processing

--- src/angular-cli-files/plugins/index-html-webpack-plugin.d.ts
+++ src/angular-cli-files/plugins/index-html-webpack-plugin.d.ts
@@ -1,12 +1,14 @@
+import { Compiler } from 'webpack';
 export interface IndexHtmlWebpackPluginOptions {
     input: string;
     output: string;
     baseHref?: string;
     entrypoints: string[];
     deployUrl?: string;
+    sri: boolean;
 }
 export declare class IndexHtmlWebpackPlugin {
     private _options;
     constructor(options?: Partial<IndexHtmlWebpackPluginOptions>);
-    apply(compiler: any): void;
+    apply(compiler: Compiler): void;
 }
