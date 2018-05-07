b4b8cbb3a41f8d4cb89c374fa0090f4016bcfa24
Angular Builds <circleci@angular.io>
2018-05-07 18:40:37 +0000

    fe12251 style: prepend Google @license to all files

--- src/angular-cli-files/models/webpack-configs/typescript.d.ts
+++ src/angular-cli-files/models/webpack-configs/typescript.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="node" />
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 import { virtualFs } from '@angular-devkit/core';
 import { Stats } from 'fs';
 import { AngularCompilerPlugin } from '@ngtools/webpack';
