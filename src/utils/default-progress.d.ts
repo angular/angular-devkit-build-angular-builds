3cfbfdf85c0ed08f8ca0b60f41587ea1101e4bb6
Angular Builds <circleci@angular.io>
2018-08-07 16:47:15 +0000

    8fd1040 fix(@angular-devkit/build-angular): use process TTY as progress default

--- src/utils/default-progress.d.ts
+++ src/utils/default-progress.d.ts
@@ -5,7 +5,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export * from './default-progress';
-export * from './run-module-as-observable-fork';
-export * from './normalize-file-replacements';
-export * from './normalize-asset-patterns';
+export declare function defaultProgress(progress: boolean | undefined): boolean;
