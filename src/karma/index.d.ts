aabb843a77348d6ab3bf040a6cba972818464c28
Angular Builds <circleci@angular.io>
2018-10-23 20:39:22 +0000

    8905a4d Branch support in snapshots (#12692)

--- src/karma/index.d.ts
+++ src/karma/index.d.ts
@@ -5,10 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/// <reference types="node" />
 import { BuildEvent, Builder, BuilderConfiguration, BuilderContext } from '@angular-devkit/architect';
-import { Path, virtualFs } from '@angular-devkit/core';
-import * as fs from 'fs';
 import { Observable } from 'rxjs';
 import { AssetPatternObject, CurrentFileReplacement } from '../browser/schema';
 import { KarmaBuilderSchema } from './schema';
@@ -20,6 +17,6 @@ export declare class KarmaBuilder implements Builder<KarmaBuilderSchema> {
     context: BuilderContext;
     constructor(context: BuilderContext);
     run(builderConfig: BuilderConfiguration<KarmaBuilderSchema>): Observable<BuildEvent>;
-    buildWebpackConfig(root: Path, projectRoot: Path, sourceRoot: Path | undefined, host: virtualFs.Host<fs.Stats>, options: NormalizedKarmaBuilderSchema): any;
+    private _buildWebpackConfig;
 }
 export default KarmaBuilder;
