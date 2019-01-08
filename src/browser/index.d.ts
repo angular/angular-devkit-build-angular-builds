9ff5440d1e273586c674a6ea0f7c3c972d97154c
Angular Builds <circleci@angular.io>
2019-01-08 22:05:43 +0000

    100cf47 feat(@schematics/angular): generate a library with a README.md file

--- src/browser/index.d.ts
+++ src/browser/index.d.ts
@@ -7,7 +7,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { BuildEvent, Builder, BuilderConfiguration, BuilderContext } from '@angular-devkit/architect';
-import { LoggingCallback, WebpackBuilder } from '@angular-devkit/build-webpack';
+import { LoggingCallback } from '@angular-devkit/build-webpack';
 import { Path, virtualFs } from '@angular-devkit/core';
 import * as fs from 'fs';
 import { Observable } from 'rxjs';
@@ -15,8 +15,6 @@ import { BrowserBuilderSchema, NormalizedBrowserBuilderSchema } from './schema';
 export declare class BrowserBuilder implements Builder<BrowserBuilderSchema> {
     context: BuilderContext;
     constructor(context: BuilderContext);
-    protected createWebpackBuilder(context: BuilderContext): WebpackBuilder;
-    protected createLoggingFactory(): (verbose: boolean) => LoggingCallback;
     run(builderConfig: BuilderConfiguration<BrowserBuilderSchema>): Observable<BuildEvent>;
     buildWebpackConfig(root: Path, projectRoot: Path, host: virtualFs.Host<fs.Stats>, options: NormalizedBrowserBuilderSchema): any;
     private _deleteOutputDir;
