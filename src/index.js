216229cadab6ed93b7e8646314adf017399c12d4
Angular Builds <circleci@angular.io>
2019-04-16 17:04:49 +0000

    403579b build: update karma to version ~4.1.0

--- src/index.js
+++ src/index.js
@@ -6,19 +6,15 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./app-shell"));
-__export(require("./browser"));
 var schema_1 = require("./browser/schema");
 exports.OutputHashing = schema_1.OutputHashing;
 exports.Type = schema_1.Type;
-__export(require("./dev-server"));
-__export(require("./extract-i18n"));
-__export(require("./karma"));
-__export(require("./protractor"));
+var browser_1 = require("./browser");
+exports.executeBrowserBuilder = browser_1.buildWebpackBrowser;
+var dev_server_1 = require("./dev-server");
+exports.executeDevServerBuilder = dev_server_1.serveWebpackBrowser;
+var karma_1 = require("./karma");
+exports.executeKarmaBuilder = karma_1.execute;
 var server_1 = require("./server");
 exports.executeServerBuilder = server_1.execute;
-__export(require("./tslint"));
