3085bb72e6bb7191c1a7fd97c74bf5ba1de78947
Angular Builds <circleci@angular.io>
2019-05-15 21:47:13 +0000

    2908985 fix(@angular/cli): prevent nested schematic execution from generating analytics

--- src/angular-cli-files/utilities/read-tsconfig.d.ts
+++ src/angular-cli-files/utilities/read-tsconfig.d.ts
@@ -1,1 +1,9 @@
-export declare function readTsconfig(tsconfigPath: string): import("typescript").ParsedCommandLine;
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as ts from 'typescript';
+export declare function readTsconfig(tsconfigPath: string): ts.ParsedCommandLine;
