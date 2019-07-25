508246df6daf396519b2929cdb3b7152475ed43e
Angular Builds <circleci@angular.io>
2019-07-25 02:29:57 +0000

    069a18200 fix(@angular-devkit/build-angular): support pnpm with ng serve

--- src/angular-cli-files/models/webpack-configs/common.js
+++ src/angular-cli-files/models/webpack-configs/common.js
@@ -321,6 +321,14 @@ function getCommonConfig(wco) {
                     parser: { system: true },
                 },
                 {
+                    test: /[\/\\]hot[\/\\]emitter.js$/,
+                    parser: { node: { events: true } },
+                },
+                {
+                    test: /[\/\\]webpack-dev-server[\/\\]client[\/\\]utils[\/\\]createSocketUrl.js$/,
+                    parser: { node: { querystring: true } },
+                },
+                {
                     test: /\.js$/,
                     ...buildOptimizerUseRule,
                 },
