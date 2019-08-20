4af921534520680a1c3f783c98dc3742280fa764
Angular Builds <circleci@angular.io>
2019-08-20 18:43:08 +0000

    d1488e66a refactor(@angular-devkit/build-angular): cache downlevel bundles

--- src/utils/process-bundle.d.ts
+++ src/utils/process-bundle.d.ts
@@ -1,12 +1,21 @@
-interface ProcessBundleOptions {
+export interface ProcessBundleOptions {
     filename: string;
     code: string;
     map?: string;
-    sourceMaps: boolean;
-    hiddenSourceMaps: boolean;
-    runtime: boolean;
+    sourceMaps?: boolean;
+    hiddenSourceMaps?: boolean;
+    vendorSourceMaps?: boolean;
+    runtime?: boolean;
     optimize: boolean;
     optimizeOnly?: boolean;
+    ignoreOriginal?: boolean;
+    cacheKeys?: (string | null)[];
+    cachePath?: string;
+}
+export declare const enum CacheKey {
+    OriginalCode = 0,
+    OriginalMap = 1,
+    DownlevelCode = 2,
+    DownlevelMap = 3
 }
 export declare function process(options: ProcessBundleOptions, callback: (error: Error | null, result?: {}) => void): void;
-export {};
