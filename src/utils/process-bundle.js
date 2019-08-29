612cd646a84369698e02facccdd3e8979edbab9e
Angular Builds <circleci@angular.io>
2019-08-29 19:10:40 +0000

    fc53087bd refactor(@angular-devkit/build-angular): add debug no mangle environment variable

--- src/utils/process-bundle.js
+++ src/utils/process-bundle.js
@@ -93,10 +93,11 @@ async function processWorker(options) {
         // estree -> terser is already supported; need babel -> estree/terser
         // Mangle downlevel code
         const result = terser_1.minify(code, {
-            compress: true,
+            compress: false,
             ecma: 5,
             mangle: !mangle_options_1.manglingDisabled,
             safari10: true,
+            toplevel: true,
             output: {
                 ascii_only: true,
                 webkit: true,
