7a68a97f635726838fbb0eed0dde090baedca754
Angular Builds <circleci@angular.io>
2019-10-23 22:01:33 +0000

    113f6fcad build: update rollup to version 1.25.2

--- src/browser/action-executor.d.ts
+++ src/browser/action-executor.d.ts
@@ -1,11 +1,15 @@
+import { I18nOptions } from '../utils/i18n-options';
 import { InlineOptions, ProcessBundleOptions, ProcessBundleResult } from '../utils/process-bundle';
 export declare class BundleActionExecutor {
     private workerOptions;
     private readonly sizeThreshold;
     private largeWorker?;
     private smallWorker?;
-    private cache;
-    constructor(workerOptions: unknown, integrityAlgorithm?: string, sizeThreshold?: number);
+    private cache?;
+    constructor(workerOptions: {
+        cachePath?: string;
+        i18n: I18nOptions;
+    }, integrityAlgorithm?: string, sizeThreshold?: number);
     private static executeMethod;
     private ensureLarge;
     private ensureSmall;
