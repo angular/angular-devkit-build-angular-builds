d39f4aaa72c66134ab73cfc03c09e4868acb54b6
Angular Builds <circleci@angular.io>
2019-10-24 10:27:42 +0000

    f6e4e0145 build: update caniuse-lite to version 1.0.30001004

---
+++ src/utils/i18n-inlining.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const path = require("path");
+function emittedFilesToInlineOptions(emittedFiles, scriptsEntryPointName, emittedPath, outputPath, es5, missingTranslation) {
+    const options = [];
+    const originalFiles = [];
+    for (const emittedFile of emittedFiles) {
+        if (emittedFile.asset ||
+            emittedFile.extension !== '.js' ||
+            (emittedFile.name && scriptsEntryPointName.includes(emittedFile.name))) {
+            continue;
+        }
+        const originalPath = path.join(emittedPath, emittedFile.file);
+        const action = {
+            filename: emittedFile.file,
+            code: fs.readFileSync(originalPath, 'utf8'),
+            es5,
+            outputPath,
+            missingTranslation,
+        };
+        originalFiles.push(originalPath);
+        try {
+            const originalMapPath = originalPath + '.map';
+            action.map = fs.readFileSync(originalMapPath, 'utf8');
+            originalFiles.push(originalMapPath);
+        }
+        catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw err;
+            }
+        }
+        options.push(action);
+    }
+    return { options, originalFiles };
+}
+exports.emittedFilesToInlineOptions = emittedFilesToInlineOptions;
