0818deeed2d61eda64076fab832fddcc056c84ff
Angular Builds <circleci@angular.io>
2019-10-31 06:12:00 +0000

    aad6ce22c build: update sass to version 1.23.2

--- src/browser/index.d.ts
+++ src/browser/index.d.ts
@@ -14,16 +14,26 @@ import { Observable } from 'rxjs';
 import * as webpack from 'webpack';
 import { IndexHtmlTransform } from '../angular-cli-files/utilities/index-file/write-index-html';
 import { ExecutionTransformer } from '../transforms';
+import { I18nOptions } from '../utils/i18n-options';
 import { Schema as BrowserBuilderSchema } from './schema';
 export declare type BrowserBuilderOutput = json.JsonObject & BuilderOutput & {
+    baseOutputPath: string;
+    outputPaths: string[];
+    /**
+     * @deprecated in version 9. Use 'outputPaths' instead.
+     */
     outputPath: string;
 };
 export declare function createBrowserLoggingCallback(verbose: boolean, logger: logging.LoggerApi): WebpackLoggingCallback;
-export declare function buildBrowserWebpackConfigFromContext(options: BrowserBuilderSchema, context: BuilderContext, host?: virtualFs.Host<fs.Stats>): Promise<{
+interface ConfigFromContextReturn {
     config: webpack.Configuration;
     projectRoot: string;
     projectSourceRoot?: string;
+}
+export declare function buildBrowserWebpackConfigFromContext(options: BrowserBuilderSchema, context: BuilderContext, host: virtualFs.Host<fs.Stats>, i18n: boolean): Promise<ConfigFromContextReturn & {
+    i18n: I18nOptions;
 }>;
+export declare function buildBrowserWebpackConfigFromContext(options: BrowserBuilderSchema, context: BuilderContext, host?: virtualFs.Host<fs.Stats>): Promise<ConfigFromContextReturn>;
 export declare function buildWebpackBrowser(options: BrowserBuilderSchema, context: BuilderContext, transforms?: {
     webpackConfiguration?: ExecutionTransformer<webpack.Configuration>;
     logging?: WebpackLoggingCallback;
--- src/utils/i18n-options.js
+++ src/utils/i18n-options.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const rimraf = require("rimraf");
+const read_tsconfig_1 = require("../angular-cli-files/utilities/read-tsconfig");
+const load_translations_1 = require("./load-translations");
 function createI18nOptions(metadata, inline) {
     if (metadata.i18n !== undefined &&
         (typeof metadata.i18n !== 'object' || !metadata.i18n || Array.isArray(metadata.i18n))) {
@@ -41,7 +47,7 @@ function createI18nOptions(metadata, inline) {
     }
     else if (inline) {
         for (const locale of inline) {
-            if (!i18n.locales[locale]) {
+            if (!i18n.locales[locale] && i18n.sourceLocale !== locale) {
                 throw new Error(`Requested inline locale '${locale}' is not defined for the project.`);
             }
             i18n.inlineLocales.add(locale);
@@ -50,6 +56,70 @@ function createI18nOptions(metadata, inline) {
     return i18n;
 }
 exports.createI18nOptions = createI18nOptions;
+async function configureI18nBuild(context, options) {
+    if (!context.target) {
+        throw new Error('The builder requires a target.');
+    }
+    const buildOptions = { ...options };
+    if (buildOptions.localize === true ||
+        (Array.isArray(buildOptions.localize) && buildOptions.localize.length > 1)) {
+        throw new Error('Using the localize option for multiple locales is temporarily disabled.');
+    }
+    const tsConfig = read_tsconfig_1.readTsconfig(buildOptions.tsConfig, context.workspaceRoot);
+    const usingIvy = tsConfig.options.enableIvy !== false;
+    const metadata = await context.getProjectMetadata(context.target);
+    const i18n = createI18nOptions(metadata, buildOptions.localize);
+    // Until 11.0, support deprecated i18n options when not using new localize option
+    // i18nFormat is automatically calculated
+    if (buildOptions.localize === undefined && usingIvy) {
+        mergeDeprecatedI18nOptions(i18n, buildOptions.i18nLocale, buildOptions.i18nFile);
+    }
+    else if (buildOptions.localize !== undefined && !usingIvy) {
+        buildOptions.localize = undefined;
+        context.logger.warn(`Option 'localize' is not supported with View Engine.`);
+    }
+    if (i18n.inlineLocales.size > 0) {
+        // Load locales
+        const loader = await load_translations_1.createTranslationLoader();
+        const projectRoot = path.join(context.workspaceRoot, metadata.root || '');
+        const usedFormats = new Set();
+        for (const [locale, desc] of Object.entries(i18n.locales)) {
+            if (i18n.inlineLocales.has(locale)) {
+                const result = loader(path.join(projectRoot, desc.file));
+                usedFormats.add(result.format);
+                if (usedFormats.size > 1 && tsConfig.options.enableI18nLegacyMessageIdFormat !== false) {
+                    // This limitation is only for legacy message id support (defaults to true as of 9.0)
+                    throw new Error('Localization currently only supports using one type of translation file format for the entire application.');
+                }
+                desc.format = result.format;
+                desc.translation = result.translation;
+            }
+        }
+        // Legacy message id's require the format of the translations
+        if (usedFormats.size > 0) {
+            buildOptions.i18nFormat = [...usedFormats][0];
+        }
+        // If only one locale is specified set the deprecated option to enable the webpack plugin
+        // transform to register the locale directly in the output bundle.
+        if (i18n.inlineLocales.size === 1) {
+            buildOptions.i18nLocale = [...i18n.inlineLocales][0];
+        }
+    }
+    // If inlining store the output in a temporary location to facilitate post-processing
+    if (i18n.shouldInline) {
+        const tempPath = fs.mkdtempSync(path.join(fs.realpathSync(os.tmpdir()), 'angular-cli-i18n-'));
+        buildOptions.outputPath = tempPath;
+        // Remove temporary directory used for i18n processing
+        process.on('exit', () => {
+            try {
+                rimraf.sync(tempPath);
+            }
+            catch (_a) { }
+        });
+    }
+    return { buildOptions, i18n };
+}
+exports.configureI18nBuild = configureI18nBuild;
 function mergeDeprecatedI18nOptions(i18n, i18nLocale, i18nFile) {
     if (i18nFile !== undefined && i18nLocale === undefined) {
         throw new Error(`Option 'i18nFile' cannot be used without the 'i18nLocale' option.`);
@@ -64,4 +134,3 @@ function mergeDeprecatedI18nOptions(i18n, i18nLocale, i18nFile) {
     }
     return i18n;
 }
-exports.mergeDeprecatedI18nOptions = mergeDeprecatedI18nOptions;
