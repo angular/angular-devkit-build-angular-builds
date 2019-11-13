622538072493a00fbf0e934459fd3143a0212a41
Angular Builds <circleci@angular.io>
2019-11-13 00:48:38 +0000

    0152c13ca build: update glob to version 7.1.6

--- src/utils/load-translations.d.ts
+++ src/utils/load-translations.d.ts
@@ -1,6 +1,5 @@
 export declare type TranslationLoader = (path: string) => {
     translation: unknown;
     format: string;
-    diagnostics: import('@angular/localize/src/tools/src/diagnostics').Diagnostics;
 };
 export declare function createTranslationLoader(): Promise<TranslationLoader>;
