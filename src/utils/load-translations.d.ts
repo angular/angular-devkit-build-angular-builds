e74338921ed7ef7f303c2855526ff9059054247b
Angular Builds <circleci@angular.io>
2019-11-13 06:54:48 +0000

    b88514206 build: update pacote to version 10.1.3

--- src/utils/load-translations.d.ts
+++ src/utils/load-translations.d.ts
@@ -1,5 +1,6 @@
 export declare type TranslationLoader = (path: string) => {
     translation: unknown;
     format: string;
+    diagnostics: import('@angular/localize/src/tools/src/diagnostics').Diagnostics;
 };
 export declare function createTranslationLoader(): Promise<TranslationLoader>;
