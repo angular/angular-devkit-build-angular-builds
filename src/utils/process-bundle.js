25d0b3337dfcd93a8fc7c6d25ee68e726e662bc2
Angular Builds <circleci@angular.io>
2019-11-20 21:55:42 +0000

    5c9fc32ae docs: update release doc with more merge details

--- src/utils/process-bundle.js
+++ src/utils/process-bundle.js
@@ -50,7 +50,7 @@ async function process(options) {
     }
     const basePath = path.dirname(options.filename);
     const filename = path.basename(options.filename);
-    const downlevelFilename = filename.replace(/\-es20\d{2}/, '-es5');
+    const downlevelFilename = filename.replace('es2015', 'es5');
     const downlevel = !options.optimizeOnly;
     // if code size is larger than 500kB, manually handle sourcemaps with newer source-map package.
     // babel currently uses an older version that still supports sync calls
@@ -263,8 +263,8 @@ async function processRuntime(options) {
     }
     // Adjust lazy loaded scripts to point to the proper variant
     // Extra spacing is intentional to align source line positions
-    downlevelCode = downlevelCode.replace(/"\-es20\d{2}\./, '   "-es5.');
-    const downlevelFilePath = options.filename.replace(/\-es20\d{2}/, '-es5');
+    downlevelCode = downlevelCode.replace('"-es2015.', '   "-es5.');
+    const downlevelFilePath = options.filename.replace('es2015', 'es5');
     let downlevelMap;
     let result;
     if (options.optimize) {
