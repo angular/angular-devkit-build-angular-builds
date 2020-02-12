8d93d723e5a78e0bb31962af2e291a19bf0fa790
Angular Builds <circleci@angular.io>
2020-02-12 20:33:46 +0000

    609187942 fix(@angular-devkit/build-angular): generate correct filenames when targeting ESNext

--- src/extract-i18n/index.d.ts
+++ src/extract-i18n/index.d.ts
@@ -6,9 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { BuilderContext } from '@angular-devkit/architect';
+import { BuildResult } from '@angular-devkit/build-webpack';
 import { JsonObject } from '@angular-devkit/core';
 import { Schema } from './schema';
 export declare type ExtractI18nBuilderOptions = Schema & JsonObject;
-export declare function execute(options: ExtractI18nBuilderOptions, context: BuilderContext): Promise<import("../../../build_webpack/src").BuildResult>;
+export declare function execute(options: ExtractI18nBuilderOptions, context: BuilderContext): Promise<BuildResult>;
 declare const _default: import("@angular-devkit/architect/src/internal").Builder<JsonObject & Schema>;
 export default _default;
