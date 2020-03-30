744e9ea0a4e57c6feee240e41f10765b9624aa5e
Angular Builds <circleci@angular.io>
2020-03-30 18:27:17 +0000

    40938e4b5 build: update angular packages

--- src/angular-cli-files/plugins/common-js-usage-warn-plugin.js
+++ src/angular-cli-files/plugins/common-js-usage-warn-plugin.js
@@ -15,11 +15,20 @@ class CommonJsUsageWarnPlugin {
     constructor(options = {}) {
         this.options = options;
         this.shownWarnings = new Set();
+        // Allow the below depedency for HMR
+        // tslint:disable-next-line: max-line-length
+        // https://github.com/angular/angular-cli/blob/1e258317b1f6ec1e957ee3559cc3b28ba602f3ba/packages/angular_devkit/build_angular/src/dev-server/index.ts#L605-L638
+        this.allowedDepedencies = [
+            'webpack/hot/dev-server',
+        ];
+        if (this.options.allowedDepedencies) {
+            this.allowedDepedencies.push(...this.options.allowedDepedencies);
+        }
     }
     apply(compiler) {
         compiler.hooks.compilation.tap('CommonJsUsageWarnPlugin', compilation => {
             compilation.hooks.finishModules.tap('CommonJsUsageWarnPlugin', modules => {
-                var _a, _b, _c;
+                var _a, _b, _c, _d;
                 for (const { dependencies, rawRequest, issuer } of modules) {
                     if (!rawRequest ||
                         rawRequest.startsWith('.') ||
@@ -27,7 +36,7 @@ class CommonJsUsageWarnPlugin {
                         // Skip if module is absolute or relative.
                         continue;
                     }
-                    if ((_a = this.options.allowedDepedencies) === null || _a === void 0 ? void 0 : _a.includes(rawRequest)) {
+                    if ((_a = this.allowedDepedencies) === null || _a === void 0 ? void 0 : _a.includes(rawRequest)) {
                         // Skip as this module is allowed even if it's a CommonJS.
                         continue;
                     }
@@ -44,7 +53,9 @@ class CommonJsUsageWarnPlugin {
                             mainIssuer = mainIssuer.issuer;
                         }
                         // Only show warnings for modules from main entrypoint.
-                        if ((mainIssuer === null || mainIssuer === void 0 ? void 0 : mainIssuer.name) === 'main') {
+                        // And if the issuer request is not from 'webpack-dev-server', as 'webpack-dev-server'
+                        // will require CommonJS libraries for live reloading such as 'sockjs-node'.
+                        if ((mainIssuer === null || mainIssuer === void 0 ? void 0 : mainIssuer.name) === 'main' && !((_d = issuer === null || issuer === void 0 ? void 0 : issuer.userRequest) === null || _d === void 0 ? void 0 : _d.includes('webpack-dev-server'))) {
                             const warning = `${issuer === null || issuer === void 0 ? void 0 : issuer.userRequest} depends on ${rawRequest}. CommonJS or AMD dependencies can cause optimization bailouts.`;
                             // Avoid showing the same warning multiple times when in 'watch' mode.
                             if (!this.shownWarnings.has(warning)) {
