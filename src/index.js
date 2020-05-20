ec8642d89fcd92df5563784c4cdc779ba2882fdf
Angular Builds <circleci@angular.io>
2020-05-20 09:41:54 +0000

    7928de118 build: update bazel packages

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 }));
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
-};
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 // TODO: remove this commented AJV require.
 // We don't actually require AJV, but there is a bug with NPM and peer dependencies that is
