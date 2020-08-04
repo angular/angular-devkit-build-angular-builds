45a3ec1825005e2531d7fbbc3c3f5b563d3dc682
Angular Builds <circleci@angular.io>
2020-08-04 16:43:47 +0000

    520459ed9 refactor: minor cleanup to common webpack configuration

--- src/angular-cli-files/models/webpack-configs/common.js
+++ src/angular-cli-files/models/webpack-configs/common.js
@@ -167,20 +167,18 @@ function getCommonConfig(wco) {
         }
         return prev;
     }, []);
-    if (globalScriptsByBundleName.length > 0) {
-        // Add a new asset for each entry.
-        globalScriptsByBundleName.forEach(script => {
-            // Lazy scripts don't get a hash, otherwise they can't be loaded by name.
-            const hash = script.inject ? hashFormat.script : '';
-            const bundleName = script.bundleName;
-            extraPlugins.push(new webpack_2.ScriptsWebpackPlugin({
-                name: bundleName,
-                sourceMap: scriptsSourceMap,
-                filename: `${path.basename(bundleName)}${hash}.js`,
-                scripts: script.paths,
-                basePath: projectRoot,
-            }));
-        });
+    // Add a new asset for each entry.
+    for (const script of globalScriptsByBundleName) {
+        // Lazy scripts don't get a hash, otherwise they can't be loaded by name.
+        const hash = script.inject ? hashFormat.script : '';
+        const bundleName = script.bundleName;
+        extraPlugins.push(new webpack_2.ScriptsWebpackPlugin({
+            name: bundleName,
+            sourceMap: scriptsSourceMap,
+            filename: `${path.basename(bundleName)}${hash}.js`,
+            scripts: script.paths,
+            basePath: projectRoot,
+        }));
     }
     // process asset entries
     if (buildOptions.assets.length) {
@@ -266,11 +264,6 @@ function getCommonConfig(wco) {
             },
         ];
     }
-    // Allow loaders to be in a node_modules nested inside the devkit/build-angular package.
-    // This is important in case loaders do not get hoisted.
-    // If this file moves to another location, alter potentialNodeModules as well.
-    const loaderNodeModules = find_up_1.findAllNodeModules(__dirname, projectRoot);
-    loaderNodeModules.unshift('node_modules');
     const extraMinimizers = [];
     if (stylesOptimization) {
         extraMinimizers.push(new webpack_2.OptimizeCssWebpackPlugin({
@@ -390,7 +383,13 @@ function getCommonConfig(wco) {
         },
         resolveLoader: {
             symlinks: !buildOptions.preserveSymlinks,
-            modules: loaderNodeModules,
+            modules: [
+                // Allow loaders to be in a node_modules nested inside the devkit/build-angular package.
+                // This is important in case loaders do not get hoisted.
+                // If this file moves to another location, alter potentialNodeModules as well.
+                'node_modules',
+                ...find_up_1.findAllNodeModules(__dirname, projectRoot),
+            ],
             plugins: [PnpWebpackPlugin.moduleLoader(module)],
         },
         context: projectRoot,
