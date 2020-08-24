4a884392a364e72c9ed7441cd3400565b413b2e8
Angular Builds <circleci@angular.io>
2020-08-24 12:22:52 +0000

    0d7e0f5eb style(@angular-devkit/build-angular): remove duplicate license header

--- src/angular-cli-files/plugins/scripts-webpack-plugin.js
+++ src/angular-cli-files/plugins/scripts-webpack-plugin.js
@@ -1,4 +1,11 @@
 "use strict";
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 // tslint:disable
 // TODO: cleanup this file, it's copied as is from Angular CLI.
 Object.defineProperty(exports, "__esModule", { value: true });
