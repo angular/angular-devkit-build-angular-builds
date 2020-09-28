f5e75d8a0815c52bbd711d62311da88883114b13
Angular Builds <circleci@angular.io>
2020-09-28 12:28:53 +0000

    92f3889f9 fix(@angular-devkit/build-angular): maxModules were deprecated and renamed to modulesSpace

--- src/webpack/configs/stats.js
+++ src/webpack/configs/stats.js
@@ -8,6 +8,7 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getStatsConfig = exports.getWebpackStatsConfig = void 0;
+const webpack_version_1 = require("../../utils/webpack-version");
 const webpackOutputOptions = {
     colors: true,
     hash: true,
@@ -28,7 +29,6 @@ const verboseWebpackOutputOptions = {
     // The verbose output will most likely be piped to a file, so colors just mess it up.
     colors: false,
     usedExports: true,
-    maxModules: Infinity,
     optimizationBailout: true,
     reasons: true,
     children: true,
@@ -39,6 +39,12 @@ const verboseWebpackOutputOptions = {
     moduleTrace: true,
     logging: 'verbose',
 };
+if (webpack_version_1.isWebpackFiveOrHigher()) {
+    verboseWebpackOutputOptions['modulesSpace'] = Infinity;
+}
+else {
+    verboseWebpackOutputOptions['maxModules'] = Infinity;
+}
 function getWebpackStatsConfig(verbose = false) {
     return verbose
         ? { ...webpackOutputOptions, ...verboseWebpackOutputOptions }
