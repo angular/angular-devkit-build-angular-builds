6813f7388e17f659979b27a67448b1691cf59290
Angular Builds <circleci@angular.io>
2020-09-29 10:11:02 +0000

    38023fe41 fix(@angular-devkit/build-angular): fixed afterOptimizeChunkAssets is deprecated in webpack 5

--- src/webpack/plugins/any-component-style-budget-checker.js
+++ src/webpack/plugins/any-component-style-budget-checker.js
@@ -12,6 +12,7 @@ const path = require("path");
 const schema_1 = require("../../browser/schema");
 const bundle_calculator_1 = require("../../utils/bundle-calculator");
 const webpack_diagnostics_1 = require("../../utils/webpack-diagnostics");
+const webpack_version_1 = require("../../utils/webpack-version");
 const PLUGIN_NAME = 'AnyComponentStyleBudgetChecker';
 /**
  * Check budget sizes for component styles by emitting a warning or error if a
@@ -23,7 +24,7 @@ class AnyComponentStyleBudgetChecker {
     }
     apply(compiler) {
         compiler.hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
-            compilation.hooks.afterOptimizeChunkAssets.tap(PLUGIN_NAME, () => {
+            const afterOptimizeChunkAssets = () => {
                 // In AOT compilations component styles get processed in child compilations.
                 // tslint:disable-next-line: no-any
                 const parentCompilation = compilation.compiler.parentCompilation;
@@ -59,7 +60,18 @@ class AnyComponentStyleBudgetChecker {
                         }
                     }
                 }
-            });
+            };
+            if (webpack_version_1.isWebpackFiveOrHigher()) {
+                // webpack 5 migration "guide"
+                // https://github.com/webpack/webpack/blob/07fc554bef5930f8577f91c91a8b81791fc29746/lib/Compilation.js#L535-L539
+                // TODO_WEBPACK_5 const stage = Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE + 1;
+                const stage = 101;
+                // tslint:disable-next-line: no-any
+                compilation.hooks.processAssets.tap({ name: PLUGIN_NAME, stage }, afterOptimizeChunkAssets);
+            }
+            else {
+                compilation.hooks.afterOptimizeChunkAssets.tap(PLUGIN_NAME, afterOptimizeChunkAssets);
+            }
         });
     }
 }
