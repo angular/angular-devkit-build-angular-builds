ac922b476767c3b11a198222dd457d76dd489586
Angular Builds <circleci@angular.io>
2020-10-14 14:45:27 +0000

    83afc6307 test(@angular/cli): update Webpack 5 E2E test to 5.1.0

--- src/webpack/configs/browser.js
+++ src/webpack/configs/browser.js
@@ -15,12 +15,7 @@ function getBrowserConfig(wco) {
             hashFuncNames: ['sha384'],
         }));
     }
-    // TODO_WEBPACK_5: Investigate build/serve issues with the `license-webpack-plugin` package
-    if (extractLicenses && webpack_version_1.isWebpackFiveOrHigher()) {
-        wco.logger.warn('Warning: License extraction is currently disabled when using Webpack 5. ' +
-            'This is temporary and will be corrected in a future update.');
-    }
-    else if (extractLicenses) {
+    if (extractLicenses) {
         const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
         extraPlugins.push(new LicenseWebpackPlugin({
             stats: {
