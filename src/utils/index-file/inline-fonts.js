74baf0faf9b756a1f22334b2bcda2f829b0ca7bc
Angular Builds <circleci@angular.io>
2020-11-12 18:52:37 +0000

    1237ddace fix(@angular-devkit/build-angular): properly handle comment removal during font inlining

--- src/utils/index-file/inline-fonts.js
+++ src/utils/index-file/inline-fonts.js
@@ -120,10 +120,10 @@ class InlineFontsProcessor {
             cssContent += await this.getResponse(url, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36" /* Chrome */);
             if (this.options.minifyInlinedCSS) {
                 cssContent = cssContent
+                    // Comments.
+                    .replace(/\/\*([\s\S]*?)\*\//g, '')
                     // New lines.
                     .replace(/\n/g, '')
-                    // Comments and new lines.
-                    .replace(/\/\*\s.+\s\*\//g, '')
                     // Safe spaces.
                     .replace(/\s?[\{\:\;]\s+/g, s => s.trim());
             }
