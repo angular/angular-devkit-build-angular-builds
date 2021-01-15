94816540f732a1adc365821f9fc59477b2ec270a
Angular Builds <circleci@angular.io>
2021-01-15 19:10:41 +0000

    748c1f844 fix(@angular-devkit/build-angular): resolve i18n outFile with normalized format

--- plugins/karma.js
+++ plugins/karma.js
@@ -6,4 +6,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-module.exports = require('../src/webpack/plugins/karma');
+module.exports = require('../src/webpack/plugins/karma/karma');
