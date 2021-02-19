3ccd642383152d5179934d106ca7ac6bc6b6ff27
Angular Builds <circleci@angular.io>
2021-02-19 14:38:41 +0000

    990e84700 fix(@angular-devkit/build-angular): force less version 3.5 math behaviour

--- src/webpack/configs/styles.js
+++ src/webpack/configs/styles.js
@@ -112,6 +112,7 @@ function getStylesConfig(wco) {
                         implementation: require('less'),
                         sourceMap: cssSourceMap,
                         lessOptions: {
+                            math: 'always',
                             javascriptEnabled: true,
                             paths: includePaths,
                         },
