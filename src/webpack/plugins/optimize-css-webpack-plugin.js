88138ec055fb4210eb252d0a1946eec7ba5cac8c
Angular Builds <circleci@angular.io>
2021-04-13 15:54:17 +0000

    c609e7104 build: update to `cssnano` to version `5.0.0`

--- src/webpack/plugins/optimize-css-webpack-plugin.js
+++ src/webpack/plugins/optimize-css-webpack-plugin.js
@@ -68,9 +68,9 @@ class OptimizeCssWebpackPlugin {
                     map: map && { annotation: false, prev: map },
                 };
                 const output = await new Promise((resolve, reject) => {
-                    // the last parameter is not in the typings
+                    // @types/cssnano are not up to date with version 5.
                     // tslint:disable-next-line: no-any
-                    cssNano.process(content, postCssOptions, cssNanoOptions)
+                    cssNano(cssNanoOptions).process(content, postCssOptions)
                         .then(resolve)
                         .catch((err) => reject(new Error(`${file} ${err.message}`)));
                 });
