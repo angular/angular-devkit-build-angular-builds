267eb8c6fe3478bf27a686c00fd8eb78bb52d2dc
Angular Builds <circleci@angular.io>
2021-05-18 17:52:13 +0000

    758780785 docs(@angular-devkit/build-angular): add more info link to `proxyConfig`

--- src/webpack/configs/common.js
+++ src/webpack/configs/common.js
@@ -348,7 +348,6 @@ function getCommonConfig(wco) {
         context: root,
         entry: entryPoints,
         output: {
-            clean: buildOptions.deleteOutputPath,
             path: path.resolve(root, buildOptions.outputPath),
             publicPath: (_a = buildOptions.deployUrl) !== null && _a !== void 0 ? _a : '',
             filename: ({ chunk }) => {
