44b442aadd77d64ce6040797507b2ccbcf640b20
Angular Builds <circleci@angular.io>
2021-06-04 16:33:44 +0000

    2b166566b fix(@angular-devkit/build-angular): show progress during re-builds

--- src/utils/spinner.js
+++ src/utils/spinner.js
@@ -6,24 +6,37 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _isTTY;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Spinner = void 0;
 const ora = require("ora");
 const color_1 = require("./color");
+const tty_1 = require("./tty");
 class Spinner {
     constructor(text) {
         /** When false, only fail messages will be displayed. */
         this.enabled = true;
+        _isTTY.set(this, tty_1.isTTY());
         this.spinner = ora({
             text,
             // The below 2 options are needed because otherwise CTRL+C will be delayed
             // when the underlying process is sync.
             hideCursor: false,
             discardStdin: false,
+            isEnabled: __classPrivateFieldGet(this, _isTTY),
         });
     }
     set text(text) {
         this.spinner.text = text;
+    }
+    get isSpinning() {
+        return this.spinner.isSpinning || !__classPrivateFieldGet(this, _isTTY);
     }
     succeed(text) {
         if (this.enabled) {
@@ -43,3 +56,4 @@ class Spinner {
     }
 }
 exports.Spinner = Spinner;
+_isTTY = new WeakMap();
