de6f5f94765f97b05ef05459d2ca8a3d4f3ceadc
Angular Builds <circleci@angular.io>
2021-06-04 07:16:16 +0000

    2539023c3 refactor(@ngtools/webpack): remove unneeded transform operation types

--- src/webpack/configs/styles.js
+++ src/webpack/configs/styles.js
@@ -81,6 +81,13 @@ function getStylesConfig(wco) {
     }
     catch {
         sassImplementation = new sass_service_1.SassWorkerImplementation();
+        extraPlugins.push({
+            apply(compiler) {
+                compiler.hooks.shutdown.tap('sass-worker', () => {
+                    sassImplementation === null || sassImplementation === void 0 ? void 0 : sassImplementation.close();
+                });
+            },
+        });
     }
     const assetNameTemplate = helpers_1.assetNameTemplateFactory(hashFormat);
     const extraPostcssPlugins = [];
