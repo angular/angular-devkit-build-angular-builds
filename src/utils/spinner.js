b9d277d88ee1f3bac770220fbc444661708d6974
Angular Builds <circleci@angular.io>
2021-06-10 16:12:50 +0000

    559a65c51 build: update Angular dependencies to 12.1 prerelease versions

--- src/utils/spinner.js
+++ src/utils/spinner.js
@@ -6,13 +6,12 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _isTTY;
+var _Spinner_isTTY;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Spinner = void 0;
 const ora = require("ora");
@@ -22,21 +21,21 @@ class Spinner {
     constructor(text) {
         /** When false, only fail messages will be displayed. */
         this.enabled = true;
-        _isTTY.set(this, tty_1.isTTY());
+        _Spinner_isTTY.set(this, tty_1.isTTY());
         this.spinner = ora({
             text,
             // The below 2 options are needed because otherwise CTRL+C will be delayed
             // when the underlying process is sync.
             hideCursor: false,
             discardStdin: false,
-            isEnabled: __classPrivateFieldGet(this, _isTTY),
+            isEnabled: __classPrivateFieldGet(this, _Spinner_isTTY, "f"),
         });
     }
     set text(text) {
         this.spinner.text = text;
     }
     get isSpinning() {
-        return this.spinner.isSpinning || !__classPrivateFieldGet(this, _isTTY);
+        return this.spinner.isSpinning || !__classPrivateFieldGet(this, _Spinner_isTTY, "f");
     }
     succeed(text) {
         if (this.enabled) {
@@ -56,4 +55,4 @@ class Spinner {
     }
 }
 exports.Spinner = Spinner;
-_isTTY = new WeakMap();
+_Spinner_isTTY = new WeakMap();
