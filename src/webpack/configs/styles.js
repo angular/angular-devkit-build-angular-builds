1fc8a9f1a703f714825badd2f5c3b67d5fbabf2f
Angular Builds <circleci@angular.io>
2021-07-30 14:20:55 +0000

    ac3fc2752 feat(@angular-devkit/build-angular): drop support for `node-sass`

--- src/webpack/configs/styles.js
+++ src/webpack/configs/styles.js
@@ -92,22 +92,14 @@ function getStylesConfig(wco) {
         wco.logger.warn('Stylus usage is deprecated and will be removed in a future major version. ' +
             'To opt-out of the deprecated behaviour, please migrate to another stylesheet language.');
     }
-    let sassImplementation;
-    try {
-        sassImplementation = require('node-sass');
-        wco.logger.warn(`'node-sass' usage is deprecated and will be removed in a future major version. ` +
-            `To opt-out of the deprecated behaviour and start using 'sass' uninstall 'node-sass'.`);
-    }
-    catch {
-        sassImplementation = new sass_service_1.SassWorkerImplementation();
-        extraPlugins.push({
-            apply(compiler) {
-                compiler.hooks.shutdown.tap('sass-worker', () => {
-                    sassImplementation === null || sassImplementation === void 0 ? void 0 : sassImplementation.close();
-                });
-            },
-        });
-    }
+    const sassImplementation = new sass_service_1.SassWorkerImplementation();
+    extraPlugins.push({
+        apply(compiler) {
+            compiler.hooks.shutdown.tap('sass-worker', () => {
+                sassImplementation === null || sassImplementation === void 0 ? void 0 : sassImplementation.close();
+            });
+        },
+    });
     const assetNameTemplate = helpers_1.assetNameTemplateFactory(hashFormat);
     const extraPostcssPlugins = [];
     // Attempt to setup Tailwind CSS
