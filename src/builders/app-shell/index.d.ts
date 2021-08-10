d9f215c60bb1e2ee76c216ba540951931044b4b3
Angular Builds <circleci@angular.io>
2021-08-10 20:23:41 +0000

    36e53c0b3 ci: update browser `circleci/browser-tools` to 1.1.3

--- src/builders/app-shell/index.d.ts
+++ src/builders/app-shell/index.d.ts
@@ -7,5 +7,5 @@
  */
 import { JsonObject } from '@angular-devkit/core';
 import { Schema as BuildWebpackAppShellSchema } from './schema';
-declare const _default: import("@angular-devkit/architect/src/internal").Builder<BuildWebpackAppShellSchema & JsonObject>;
+declare const _default: import("@angular-devkit/architect/src/internal").Builder<JsonObject & BuildWebpackAppShellSchema>;
 export default _default;
