0d1abdbd92a782ce0772a5d777cf8e5e348c9cb1
Angular Builds <circleci@angular.io>
2022-01-31 19:19:13 +0000

    ed1322217 docs: release notes for the v13.2.1 release

--- src/webpack/plugins/css-optimizer-plugin.js
+++ src/webpack/plugins/css-optimizer-plugin.js
@@ -115,10 +115,10 @@ class CssOptimizerPlugin {
             // browserslist uses the name `ios_saf` for iOS Safari whereas esbuild uses `ios`
             if (browserName === 'ios_saf') {
                 browserName = 'ios';
-                // browserslist also uses ranges for iOS Safari versions but only the lowest is required
-                // to perform minimum supported feature checks. esbuild also expects a single version.
-                [version] = version.split('-');
             }
+            // browserslist uses ranges `15.2-15.3` versions but only the lowest is required
+            // to perform minimum supported feature checks. esbuild also expects a single version.
+            [version] = version.split('-');
             if (esBuildSupportedBrowsers.has(browserName)) {
                 if (browserName === 'safari' && version === 'TP') {
                     // esbuild only supports numeric versions so `TP` is converted to a high number (999) since
