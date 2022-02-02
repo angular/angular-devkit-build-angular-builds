3943900f222a9f3c9dcea23d8f4fa3e83e6d52b3
Angular Builds <circleci@angular.io>
2022-02-02 01:33:32 +0000

    7212252b5 test(@angular/cli): remove hardcoded versions from ng-update test

--- src/builders/karma/schema.d.ts
+++ src/builders/karma/schema.d.ts
@@ -28,7 +28,7 @@ export interface Schema {
      * - when a path to directory is provided, all spec files ending ".spec.@(ts|tsx)" will be
      * included
      * - when a path to a file is provided, and a matching spec file exists it will be included
-     * instead
+     * instead.
      */
     include?: string[];
     /**
