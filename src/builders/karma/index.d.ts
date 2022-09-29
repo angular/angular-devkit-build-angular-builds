e23ebce92cff77e51931f92947bf72033d73efdf
Angular Builds <circleci@angular.io>
2022-09-29 12:53:16 +0000

    8c3577d07 build: update all non-major dependencies

--- src/builders/karma/index.d.ts
+++ src/builders/karma/index.d.ts
@@ -23,5 +23,5 @@ export declare function execute(options: KarmaBuilderOptions, context: BuilderCo
     karmaOptions?: (options: KarmaConfigOptions) => KarmaConfigOptions;
 }): Observable<BuilderOutput>;
 export { KarmaBuilderOptions };
-declare const _default: import("../../../../architect/src/internal").Builder<Record<string, string> & KarmaBuilderOptions & import("../../../../core/src").JsonObject>;
+declare const _default: import("../../../../architect/src/internal").Builder<Record<string, string> & KarmaBuilderOptions & import("@angular-devkit/core").JsonObject>;
 export default _default;
