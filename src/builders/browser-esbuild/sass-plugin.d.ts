f5a470067fe0742c6163d535b2ca7d41a3703ffe
Angular Builds <circleci@angular.io>
2022-11-01 18:01:30 +0000

    942d555cf fix(@angular-devkit/build-angular): provide workaround for V8 object spread performance defect

--- src/builders/browser-esbuild/sass-plugin.d.ts
+++ src/builders/browser-esbuild/sass-plugin.d.ts
@@ -6,6 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import type { Plugin } from 'esbuild';
+export declare function shutdownSassWorkerPool(): void;
 export declare function createSassPlugin(options: {
     sourcemap: boolean;
     loadPaths?: string[];
