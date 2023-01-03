7af44fac5aaa6525f3d3740d0a8136c3de00facd
Angular Builds <circleci@angular.io>
2023-01-03 15:42:13 +0000

    c2030dec7 fix(@angular-devkit/build-angular): format esbuild error messages to include more information

--- src/webpack/plugins/javascript-optimizer-worker.d.ts
+++ src/webpack/plugins/javascript-optimizer-worker.d.ts
@@ -82,7 +82,13 @@ interface OptimizeRequest {
  */
 export default function ({ asset, options }: OptimizeRequest): Promise<{
     name: string;
+    errors: string[];
+    code?: undefined;
+    map?: undefined;
+} | {
+    name: string;
     code: string;
     map: import("@ampproject/remapping/dist/types/source-map").default | undefined;
+    errors?: undefined;
 }>;
 export {};
