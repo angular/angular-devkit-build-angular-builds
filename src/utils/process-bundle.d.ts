51dd300386fefe893b1b6a7e59fadb30eea952f0
Angular Builds <circleci@angular.io>
2023-01-06 16:15:21 +0000

    57a8cb604 build: update angular

--- src/utils/process-bundle.d.ts
+++ src/utils/process-bundle.d.ts
@@ -5,9 +5,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+/// <reference path="../../../../../../../../packages/angular_devkit/build_angular/src/babel-bazel.d.ts" />
+/// <reference types="@angular/compiler-cli/private/babel" />
 export declare function createI18nPlugins(locale: string, translation: unknown | undefined, missingTranslation: 'error' | 'warning' | 'ignore', shouldInline: boolean, localeDataContent?: string): Promise<{
     diagnostics: import("@angular/localize/tools").Diagnostics;
-    plugins: import("@angular/localize/tools/src/babel_core").PluginObj[];
+    plugins: import("@types/babel__core").PluginObj<import("@types/babel__core").PluginPass>[];
 }>;
 export interface InlineOptions {
     filename: string;
