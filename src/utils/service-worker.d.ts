7573ad1760d7b0c4d60eb2e797a596ef8f792cb9
Angular Builds <circleci@angular.io>
2023-03-21 17:38:47 +0000

    78239901b refactor(@angular-devkit/build-angular): consolidate result file writes in esbuild builder

--- src/utils/service-worker.d.ts
+++ src/utils/service-worker.d.ts
@@ -5,8 +5,24 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import type { Config } from '@angular/service-worker/config';
-import { promises as fsPromises } from 'fs';
+import type { Config, Filesystem } from '@angular/service-worker/config';
+import type { OutputFile } from 'esbuild';
+import { promises as fsPromises } from 'node:fs';
 export declare function augmentAppWithServiceWorker(appRoot: string, workspaceRoot: string, outputPath: string, baseHref: string, ngswConfigPath?: string, inputputFileSystem?: typeof fsPromises, outputFileSystem?: typeof fsPromises): Promise<void>;
-export declare function augmentAppWithServiceWorkerEsbuild(workspaceRoot: string, configPath: string, outputPath: string, baseHref: string): Promise<void>;
-export declare function augmentAppWithServiceWorkerCore(config: Config, outputPath: string, baseHref: string, inputputFileSystem?: typeof fsPromises, outputFileSystem?: typeof fsPromises): Promise<void>;
+export declare function augmentAppWithServiceWorkerEsbuild(workspaceRoot: string, configPath: string, baseHref: string, outputFiles: OutputFile[], assetFiles: {
+    source: string;
+    destination: string;
+}[]): Promise<{
+    manifest: string;
+    assetFiles: {
+        source: string;
+        destination: string;
+    }[];
+}>;
+export declare function augmentAppWithServiceWorkerCore(config: Config, serviceWorkerFilesystem: Filesystem, baseHref: string): Promise<{
+    manifest: string;
+    assetFiles: {
+        source: string;
+        destination: string;
+    }[];
+}>;
