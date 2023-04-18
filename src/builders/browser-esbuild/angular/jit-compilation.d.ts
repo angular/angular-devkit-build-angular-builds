85de4ec2c70e8418413d0429f645c86c1a94fedd
Angular Builds <circleci@angular.io>
2023-04-18 11:16:34 +0000

    98cc19155 refactor(@angular-devkit/build-angular): move TS/NG configuration reading into compiler classes

--- src/builders/browser-esbuild/angular/jit-compilation.d.ts
+++ src/builders/browser-esbuild/angular/jit-compilation.d.ts
@@ -5,13 +5,15 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+import type ng from '@angular/compiler-cli';
 import ts from 'typescript';
 import { AngularCompilation, FileEmitter } from './angular-compilation';
 import { AngularHostOptions } from './angular-host';
 export declare class JitCompilation extends AngularCompilation {
     #private;
-    initialize(rootNames: string[], compilerOptions: ts.CompilerOptions, hostOptions: AngularHostOptions, configurationDiagnostics?: ts.Diagnostic[]): Promise<{
+    initialize(tsconfig: string, hostOptions: AngularHostOptions, compilerOptionsTransformer?: (compilerOptions: ng.CompilerOptions) => ng.CompilerOptions): Promise<{
         affectedFiles: ReadonlySet<ts.SourceFile>;
+        compilerOptions: ng.CompilerOptions;
     }>;
     collectDiagnostics(): Iterable<ts.Diagnostic>;
     createFileEmitter(onAfterEmit?: (sourceFile: ts.SourceFile) => void): FileEmitter;
