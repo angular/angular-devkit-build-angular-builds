5053f5a2e90d5396a67e49c0b1ec0f5dc6a453fe
Angular Builds <circleci@angular.io>
2023-05-08 16:03:06 +0000

    892fcc689 fix(@angular-devkit/build-angular): convert dev-server glob proxy entries for esbuild builder

--- src/builders/dev-server/load-proxy-config.d.ts
+++ src/builders/dev-server/load-proxy-config.d.ts
@@ -6,3 +6,8 @@
  * found in the LICENSE file at https://angular.io/license
  */
 export declare function loadProxyConfiguration(root: string, proxyConfig: string | undefined): Promise<any>;
+/**
+ * Converts glob patterns to regular expressions to support Vite's proxy option.
+ * @param proxy A proxy configuration object.
+ */
+export declare function normalizeProxyConfiguration(proxy: Record<string, unknown>): void;
