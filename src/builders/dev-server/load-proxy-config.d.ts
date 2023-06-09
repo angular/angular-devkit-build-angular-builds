bdf56988cc9ee3fa9d1e63e86b5920758ea43f1c
Angular Builds <circleci@angular.io>
2023-06-09 13:41:10 +0000

    2a3fc6846 feat(@angular-devkit/build-angular): add preload hints based on transitive initial files

--- src/builders/dev-server/load-proxy-config.d.ts
+++ src/builders/dev-server/load-proxy-config.d.ts
@@ -5,4 +5,9 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export declare function loadProxyConfiguration(root: string, proxyConfig: string | undefined, normalize?: boolean): Promise<any>;
+export declare function loadProxyConfiguration(root: string, proxyConfig: string | undefined): Promise<any>;
+/**
+ * Converts glob patterns to regular expressions to support Vite's proxy option.
+ * @param proxy A proxy configuration object.
+ */
+export declare function normalizeProxyConfiguration(proxy: Record<string, unknown>): void;
