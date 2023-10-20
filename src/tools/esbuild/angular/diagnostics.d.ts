de8f1d164454878bd6d9deb74119cf8862d5e21e
Angular Builds <circleci@angular.io>
2023-10-20 18:46:31 +0000

    6029c7580 refactor(@angular-devkit/build-angular): workaround for piscina worker destroy recreation

--- src/tools/esbuild/angular/diagnostics.d.ts
+++ src/tools/esbuild/angular/diagnostics.d.ts
@@ -6,10 +6,10 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import type { PartialMessage } from 'esbuild';
-import type ts from 'typescript';
+import { Diagnostic } from 'typescript';
 /**
  * Converts a TypeScript Diagnostic message into an esbuild compatible message object.
  * @param diagnostic The TypeScript diagnostic to convert.
  * @returns An esbuild diagnostic message as a PartialMessage object
  */
-export declare function convertTypeScriptDiagnostic(typescript: typeof ts, diagnostic: ts.Diagnostic): PartialMessage;
+export declare function convertTypeScriptDiagnostic(diagnostic: Diagnostic): PartialMessage;
