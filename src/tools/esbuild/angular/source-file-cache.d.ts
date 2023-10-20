48563a27329185c3b13ef3fa10fb02054a9feed1
Angular Builds <circleci@angular.io>
2023-10-20 18:48:26 +0000

    569e2462c build: update angular

--- src/tools/esbuild/angular/source-file-cache.d.ts
+++ src/tools/esbuild/angular/source-file-cache.d.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-import ts from 'typescript';
+import type ts from 'typescript';
 import { MemoryLoadResultCache } from '../load-result-cache';
 export declare class SourceFileCache extends Map<string, ts.SourceFile> {
     readonly persistentCachePath?: string | undefined;
