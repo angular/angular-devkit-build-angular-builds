e1ebc9d2315c318e612a167b84e10da00fc1daf5
Angular Builds <circleci@angular.io>
2023-10-25 10:53:18 +0000

    5b3541040 fix(@angular-devkit/build-angular): handle HTTP requests to assets during prerendering

--- src/utils/server-rendering/render-worker.d.ts
+++ src/utils/server-rendering/render-worker.d.ts
@@ -10,9 +10,11 @@ import { RenderResult, ServerContext } from './render-page';
 export interface RenderWorkerData extends ESMInMemoryFileLoaderWorkerData {
     document: string;
     inlineCriticalCss?: boolean;
+    baseUrl: string;
 }
 export interface RenderOptions {
     route: string;
     serverContext: ServerContext;
 }
+/** Renders an application based on a provided options. */
 export default function (options: RenderOptions): Promise<RenderResult>;
