0009d92cd3b0dc4825353c6b69f9b0730d551e98
Angular Builds <circleci@angular.io>
2023-12-04 08:37:36 +0000

    aa9e7c6 fix(@angular-devkit/build-angular): normalize locale tags with Intl API when resolving in application builder

--- src/tools/esbuild/i18n-locale-plugin.d.ts
+++ src/tools/esbuild/i18n-locale-plugin.d.ts
@@ -7,6 +7,10 @@
  */
 import type { Plugin } from 'esbuild';
 /**
+ * The internal namespace used by generated locale import statements and Angular locale data plugin.
+ */
+export declare const LOCALE_DATA_NAMESPACE = "angular:locale/data";
+/**
  * The base module location used to search for locale specific data.
  */
 export declare const LOCALE_DATA_BASE_MODULE = "@angular/common/locales/global";
