03fb1a44ca47c67af007e08626ccf2559e4959f3
Angular Builds <circleci@angular.io>
2023-12-04 17:34:23 +0000

    caf7aee test: correct misconfigured saucelabs platform/OS combination

--- src/tools/esbuild/i18n-locale-plugin.js
+++ src/tools/esbuild/i18n-locale-plugin.js
@@ -7,11 +7,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createAngularLocaleDataPlugin = exports.LOCALE_DATA_BASE_MODULE = exports.LOCALE_DATA_NAMESPACE = void 0;
-/**
- * The internal namespace used by generated locale import statements and Angular locale data plugin.
- */
-exports.LOCALE_DATA_NAMESPACE = 'angular:locale/data';
+exports.createAngularLocaleDataPlugin = exports.LOCALE_DATA_BASE_MODULE = void 0;
 /**
  * The base module location used to search for locale specific data.
  */
@@ -37,37 +33,12 @@ function createAngularLocaleDataPlugin() {
             }
             build.onResolve({ filter: /^angular:locale\/data:/ }, async ({ path }) => {
                 // Extract the locale from the path
-                const rawLocaleTag = path.split(':', 3)[2];
-                // Extract and normalize the base name of the raw locale tag
-                let partialLocaleTag;
-                try {
-                    const locale = new Intl.Locale(rawLocaleTag);
-                    partialLocaleTag = locale.baseName;
-                }
-                catch {
-                    return {
-                        path: rawLocaleTag,
-                        namespace: exports.LOCALE_DATA_NAMESPACE,
-                        errors: [
-                            {
-                                text: `Invalid or unsupported locale provided in configuration: "${rawLocaleTag}"`,
-                            },
-                        ],
-                    };
-                }
+                const originalLocale = path.split(':', 3)[2];
+                // Remove any private subtags since these will never match
+                let partialLocale = originalLocale.replace(/-x(-[a-zA-Z0-9]{1,8})+$/, '');
                 let exact = true;
-                while (partialLocaleTag) {
-                    // Angular embeds the `en`/`en-US` locale into the framework and it does not need to be included again here.
-                    // The onLoad hook below for the locale data namespace has an `empty` loader that will prevent inclusion.
-                    // Angular does not contain exact locale data for `en-US` but `en` is equivalent.
-                    if (partialLocaleTag === 'en' || partialLocaleTag === 'en-US') {
-                        return {
-                            path: rawLocaleTag,
-                            namespace: exports.LOCALE_DATA_NAMESPACE,
-                        };
-                    }
-                    // Attempt to resolve the locale tag data within the Angular base module location
-                    const potentialPath = `${exports.LOCALE_DATA_BASE_MODULE}/${partialLocaleTag}`;
+                while (partialLocale) {
+                    const potentialPath = `${exports.LOCALE_DATA_BASE_MODULE}/${partialLocale}`;
                     const result = await build.resolve(potentialPath, {
                         kind: 'import-statement',
                         resolveDir: build.initialOptions.absWorkingDir,
@@ -83,37 +54,36 @@ function createAngularLocaleDataPlugin() {
                                     ...result.warnings,
                                     {
                                         location: null,
-                                        text: `Locale data for '${rawLocaleTag}' cannot be found. Using locale data for '${partialLocaleTag}'.`,
+                                        text: `Locale data for '${originalLocale}' cannot be found. Using locale data for '${partialLocale}'.`,
                                     },
                                 ],
                             };
                         }
                     }
-                    // Remove the last subtag and try again with a less specific locale.
-                    // Usually the match is exact so the string splitting here is not done until actually needed after the exact
-                    // match fails to resolve.
-                    const parts = partialLocaleTag.split('-');
-                    partialLocaleTag = parts.slice(0, -1).join('-');
+                    // Remove the last subtag and try again with a less specific locale
+                    const parts = partialLocale.split('-');
+                    partialLocale = parts.slice(0, -1).join('-');
                     exact = false;
+                    // The locales "en" and "en-US" are considered exact to retain existing behavior
+                    if (originalLocale === 'en-US' && partialLocale === 'en') {
+                        exact = true;
+                    }
                 }
                 // Not found so issue a warning and use an empty loader. Framework built-in `en-US` data will be used.
                 // This retains existing behavior as in the Webpack-based builder.
                 return {
-                    path: rawLocaleTag,
-                    namespace: exports.LOCALE_DATA_NAMESPACE,
+                    path: originalLocale,
+                    namespace: 'angular:locale/data',
                     warnings: [
                         {
                             location: null,
-                            text: `Locale data for '${rawLocaleTag}' cannot be found. No locale data will be included for this locale.`,
+                            text: `Locale data for '${originalLocale}' cannot be found. No locale data will be included for this locale.`,
                         },
                     ],
                 };
             });
             // Locales that cannot be found will be loaded as empty content with a warning from the resolve step
-            build.onLoad({ filter: /./, namespace: exports.LOCALE_DATA_NAMESPACE }, () => ({
-                contents: '',
-                loader: 'empty',
-            }));
+            build.onLoad({ filter: /./, namespace: 'angular:locale/data' }, () => ({ loader: 'empty' }));
         },
     };
 }
