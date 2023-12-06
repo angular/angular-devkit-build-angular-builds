daf08cee8751383e57d81d876db08998aa151f61
Angular Builds <circleci@angular.io>
2023-12-06 16:30:41 +0000

    10588c2 refactor(@angular-devkit/build-angular): update Node.js module register load hook for new types

--- src/utils/server-rendering/esm-in-memory-loader/register-hooks.js
+++ src/utils/server-rendering/esm-in-memory-loader/register-hooks.js
@@ -7,9 +7,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-// TODO: remove the below once @types/node are version 20.x.x
-// @ts-expect-error "node:module"' has no exported member 'register'.ts(2305)
 const node_module_1 = require("node:module");
 const node_url_1 = require("node:url");
 const node_worker_threads_1 = require("node:worker_threads");
-(0, node_module_1.register)('./loader-hooks.js', (0, node_url_1.pathToFileURL)(__filename), { data: node_worker_threads_1.workerData });
+(0, node_module_1.register)('./loader-hooks.js', { parentURL: (0, node_url_1.pathToFileURL)(__filename), data: node_worker_threads_1.workerData });
