8b1b08f705d3784f68c435ec250628f3eafe200c
Angular Builds <circleci@angular.io>
2023-12-20 15:02:26 +0000

    ceffafe fix(@angular-devkit/build-angular): provide better error messages for failed file reads

--- src/utils/index-file/index-html-generator.js
+++ src/utils/index-file/index-html-generator.js
@@ -6,33 +6,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.IndexHtmlGenerator = void 0;
-const fs = __importStar(require("fs"));
-const path_1 = require("path");
+const promises_1 = require("node:fs/promises");
+const node_path_1 = require("node:path");
 const strip_bom_1 = require("../strip-bom");
 const augment_index_html_1 = require("./augment-index-html");
 const inline_critical_css_1 = require("./inline-critical-css");
@@ -85,10 +62,20 @@ class IndexHtmlGenerator {
         };
     }
     async readAsset(path) {
-        return fs.promises.readFile(path, 'utf-8');
+        try {
+            return await (0, promises_1.readFile)(path, 'utf-8');
+        }
+        catch {
+            throw new Error(`Failed to read asset "${path}".`);
+        }
     }
     async readIndex(path) {
-        return fs.promises.readFile(path, 'utf-8');
+        try {
+            return await (0, promises_1.readFile)(path, 'utf-8');
+        }
+        catch {
+            throw new Error(`Failed to read index HTML file "${path}".`);
+        }
     }
 }
 exports.IndexHtmlGenerator = IndexHtmlGenerator;
@@ -104,7 +91,7 @@ function augmentIndexHtmlPlugin(generator) {
             sri,
             lang,
             entrypoints,
-            loadOutputFile: (filePath) => generator.readAsset((0, path_1.join)(outputPath, filePath)),
+            loadOutputFile: (filePath) => generator.readAsset((0, node_path_1.join)(outputPath, filePath)),
             imageDomains,
             files,
             hints,
