2288435b3373bc02c3b35768c2e0358168c195b1
Angular Builds <circleci@angular.io>
2024-04-17 13:45:30 +0000

    1c3ff61 fix(@angular-devkit/build-angular): address `Unable to deserialize cloned data` issue with Yarn PnP

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-next.2+sha-6313036';
+const VERSION = '18.0.0-next.2+sha-1c3ff61';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
