0df3a5b7526ec07b3ef541d8299da6c655348cb5
Angular Builds <circleci@angular.io>
2024-04-24 11:13:27 +0000

    b2ac5fa feat(@schematics/angular): allow application migration to use new build package in projects where possible

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-next.3+sha-7cedcc8';
+const VERSION = '18.0.0-next.3+sha-b2ac5fa';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
