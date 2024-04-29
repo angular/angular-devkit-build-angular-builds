6f64d04340100e3026ef0a51a4d7e0b77fdd63f5
Angular Builds <circleci@angular.io>
2024-04-29 18:09:33 +0000

    3dae3c0 fix(@schematics/angular): add less dependency in application migration if needed

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.0+sha-b0ec254';
+const VERSION = '18.1.0-next.0+sha-3dae3c0';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
