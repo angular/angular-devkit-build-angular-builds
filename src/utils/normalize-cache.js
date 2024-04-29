269a2fbace4fce447dff690e2a5a3f39f3aa3235
Angular Builds <circleci@angular.io>
2024-04-29 18:26:57 +0000

    44b4017 fix(@angular/build): ensure input index HTML file triggers rebuilds when changed

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-next.5+sha-95a4d6e';
+const VERSION = '18.0.0-next.5+sha-44b4017';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
