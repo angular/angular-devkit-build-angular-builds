e9aee958f093b71ae6a9b92b7a92dec7f9fc0bac
Angular Builds <circleci@angular.io>
2024-05-01 19:37:26 +0000

    c119996 build: replace `@angular/build` version with archive in local builds

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-rc.0+sha-3d52648';
+const VERSION = '18.0.0-rc.0+sha-c119996';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
