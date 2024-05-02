b4981f73013154764d3a035fdd7d57ba8c965ec3
Angular Builds <circleci@angular.io>
2024-05-02 15:01:04 +0000

    b7a0792 fix(@angular/build): add a maximum rendering timeout for SSG

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-rc.0+sha-a8a4d1f';
+const VERSION = '18.0.0-rc.0+sha-b7a0792';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
