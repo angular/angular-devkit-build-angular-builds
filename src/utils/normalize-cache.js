77d9ed9ec61b66e754489d15573ce0ebe4b6b42e
Angular Builds <circleci@angular.io>
2024-05-03 16:21:55 +0000

    53cb8b9 build: replace lodash.template with lodash v4.17.21

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-rc.1+sha-12a3da7';
+const VERSION = '18.0.0-rc.1+sha-53cb8b9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
