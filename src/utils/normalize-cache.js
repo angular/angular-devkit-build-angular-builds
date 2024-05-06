efa9420018be382ac607256268d467ad77dfde25
Angular Builds <circleci@angular.io>
2024-05-06 14:07:35 +0000

    9e13a8b fix(@angular/build): format sizes using decimal byte units consistently

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.0+sha-1d2f642';
+const VERSION = '18.1.0-next.0+sha-9e13a8b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
