c28e20dc05c0894bf9ed6285a47aae86b9e9a823
Angular Builds <circleci@angular.io>
2024-05-08 07:00:55 +0000

    7223c3f refactor(@angular/build): show unexpected Sass import resolution errors

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.0+sha-03c09c9';
+const VERSION = '18.1.0-next.0+sha-7223c3f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
