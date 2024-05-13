fda62a229218a96a43aaf0a22782dfb5842632e6
Angular Builds <circleci@angular.io>
2024-05-13 12:05:10 +0000

    fa99a01 fix(@angular/cli): clarify optional migration instructions during ng update

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-rc.2+sha-58a8197';
+const VERSION = '18.0.0-rc.2+sha-fa99a01';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
