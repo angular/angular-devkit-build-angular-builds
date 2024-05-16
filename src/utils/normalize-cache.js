da6c8ea93c59ed25a781c5ed50e6deae9825dd1e
Angular Builds <circleci@angular.io>
2024-05-16 23:25:04 +0000

    2265f63 refactor(@angular/build): cleanup persistent JS transformer cache on plugin dispose

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.0-rc.2+sha-dc20948';
+const VERSION = '18.0.0-rc.2+sha-2265f63';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
