6e8613c5db42efbf269fab95d34d88b9617e07c5
Angular Builds <circleci@angular.io>
2024-05-20 21:14:06 +0000

    ebfd7b1 refactor(@angular/cli): show optional migration name and documentation URL if available during updates

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.0+sha-35ac47e';
+const VERSION = '18.1.0-next.0+sha-ebfd7b1';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
