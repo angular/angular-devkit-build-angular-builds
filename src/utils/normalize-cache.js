c968775b9aede93f4b4024eaa45b9e103aff1858
Angular Builds <circleci@angular.io>
2024-05-27 06:15:22 +0000

    2a3d7a0 ci: always run snapshot E2E tests on main CI

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.1+sha-9967c04';
+const VERSION = '18.0.1+sha-2a3d7a0';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
