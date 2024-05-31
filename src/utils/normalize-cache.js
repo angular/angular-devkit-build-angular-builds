6cf4717aba71375e4e4cb43c163365843bbcc918
Angular Builds <circleci@angular.io>
2024-05-31 19:16:51 +0000

    9acb5c7 fix(@angular/build): disable JS transformer persistent cache on web containers

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.2+sha-25624af';
+const VERSION = '18.0.2+sha-9acb5c7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
