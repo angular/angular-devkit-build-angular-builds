f8e0d601950aa09efef6f0978894223bace5cc69
Angular Builds <circleci@angular.io>
2024-06-05 14:10:24 +0000

    cd1c181 ci: update dependency-review-config - add `CC0-1.0` as valid licenses - excludes `thingies@1.21.0` which is `MIT and Unlicense`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.0+sha-c92bc77';
+const VERSION = '18.1.0-next.0+sha-cd1c181';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
