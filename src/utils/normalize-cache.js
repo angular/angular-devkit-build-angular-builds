9de11e00c4f5d08a115cc35e9df6b03beb0e6e8a
Angular Builds <circleci@angular.dev>
2024-06-17 10:43:41 +0000

    5c705e8 fix(@schematics/angular): update schematics to use RouterModule when --routing flag is present

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.4+sha-405c148';
+const VERSION = '18.0.4+sha-5c705e8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
