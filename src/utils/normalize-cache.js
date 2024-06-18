9ec2c9b3a48ea55a3dc887377240f10af28c313b
Angular Builds <circleci@angular.dev>
2024-06-18 07:43:42 +0000

    a0f7d15 ci: remove ci testing push trigger

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = void 0;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.0.4+sha-5c705e8';
+const VERSION = '18.0.4+sha-a0f7d15';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
