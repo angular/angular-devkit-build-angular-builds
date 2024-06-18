ac10f7d8a2841548cc821a541e5342e5164b2746
Angular Builds <circleci@angular.dev>
2024-06-18 14:01:13 +0000

    c0ceddf fix(@angular/build): add CSP nonce to script with src tags

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.2+sha-a1f60f9';
+const VERSION = '18.1.0-next.2+sha-c0ceddf';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
