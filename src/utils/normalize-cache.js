64ed1dbdc1e8a7e3b0d506eee3f68b4eaa3ada53
Angular Builds <circleci@angular.dev>
2024-06-20 12:53:18 +0000

    094efca docs: release notes for the v18.0.5 release

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.2+sha-9c647a7';
+const VERSION = '18.1.0-next.2+sha-094efca';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
