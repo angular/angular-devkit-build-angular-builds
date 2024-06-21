8707bf2a4717f85489c40f6867013857285b22ab
Angular Builds <circleci@angular.dev>
2024-06-21 14:31:45 +0000

    db1a6ae refactor(@angular/build): move generic Angular tools into separate source directory

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.0-next.3+sha-438b530';
+const VERSION = '18.1.0-next.3+sha-db1a6ae';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
