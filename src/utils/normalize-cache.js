5356bff54cbc331e9b1e7e2221f58d98aeec8190
Angular Builds <circleci@angular.dev>
2024-06-27 11:32:28 +0000

    e6d5c7e refactor(@angular/build): improve BuildOutputFile property access

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.0-next.0+sha-ab6ccee';
+const VERSION = '18.2.0-next.0+sha-e6d5c7e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
