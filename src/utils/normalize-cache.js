80c8c3a3f5b4e3f009e86ffa3407ec1bc75cd703
Angular Builds <circleci@angular.dev>
2024-07-07 02:43:58 +0000

    2cb1fb3 feat(@angular/build): support WASM/ES Module integration proposal

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.0-next.0+sha-aa88e68';
+const VERSION = '18.2.0-next.0+sha-2cb1fb3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
