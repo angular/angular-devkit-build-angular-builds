10ebc91049feb826afcdf43d1b802dc45218f200
Angular Builds <circleci@angular.dev>
2024-07-12 08:18:04 +0000

    9baae6e fix(@angular-devkit/build-angular): skip undefined files when generating budget stats

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.0-next.0+sha-ba13d03';
+const VERSION = '18.2.0-next.0+sha-9baae6e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
