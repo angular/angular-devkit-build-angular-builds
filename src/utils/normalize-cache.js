ee38b56c5a150ba393f652efa1f34af9f005761e
Angular Builds <circleci@angular.dev>
2024-07-15 12:12:40 +0000

    24aaf1e feat(@angular/build): support import attribute based loader configuration

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.0-next.0+sha-6f521fb';
+const VERSION = '18.2.0-next.0+sha-24aaf1e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
