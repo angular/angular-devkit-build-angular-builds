a5784b90a3f21d708ef3f2bb45b4aa23856eda23
Angular Builds <circleci@angular.dev>
2024-07-19 14:41:01 +0000

    5b9378a fix(@angular/build): account for HTML base HREF for dev-server externals

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.1+sha-d868270';
+const VERSION = '18.1.1+sha-5b9378a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
