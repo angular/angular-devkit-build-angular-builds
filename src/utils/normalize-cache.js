4fa7200cc43a15432147b38d0e977e61655f33d6
Angular Builds <circleci@angular.dev>
2024-07-19 16:57:01 +0000

    3e4ea77 fix(@angular/build): correctly detect comma in Sass URL lexer

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.1.1+sha-5b9378a';
+const VERSION = '18.1.1+sha-3e4ea77';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
