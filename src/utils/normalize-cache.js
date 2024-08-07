23146e9acc53a22725a78a5afc30fe02fc1182c9
Angular Builds <circleci@angular.dev>
2024-08-07 15:48:58 +0000

    34908a3 fix(@angular/build): lazy load Node.js inspector for dev server

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.0-next.3+sha-c4fa7b6';
+const VERSION = '18.2.0-next.3+sha-34908a3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
