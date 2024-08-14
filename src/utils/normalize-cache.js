1ca474c914ee6da3caa6fe4aa932650a7c0037af
Angular Builds <circleci@angular.dev>
2024-08-14 17:16:46 +0000

    0c2ca9a build: remove Bazel typings workaround

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.0-rc.0+sha-4528be1';
+const VERSION = '18.2.0-rc.0+sha-0c2ca9a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
