cf8c503991b7110ddf507858f87bb6d080255204
Angular Builds <circleci@angular.dev>
2024-08-15 12:04:38 +0000

    e043c96 build: update github/codeql-action action to v3.26.2

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.0+sha-a7f466a';
+const VERSION = '19.0.0-next.0+sha-e043c96';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
