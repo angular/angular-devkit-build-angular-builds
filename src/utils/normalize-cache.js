0ca7b6cf99df5d2e3d7d19cca7971a0cc90097aa
Angular Builds <circleci@angular.dev>
2024-08-20 17:58:11 +0000

    e72eb05 refactor(@angular/build): template style elements should always be CSS

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.0+sha-0676e2a';
+const VERSION = '19.0.0-next.0+sha-e72eb05';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
