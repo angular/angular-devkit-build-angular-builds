de8214536fdeaa616bc8c118aa8407efcfe36212
Angular Builds <circleci@angular.dev>
2024-08-22 19:22:24 +0000

    c279523 build: update material packages to 19.0.0-next.1

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.1+sha-4aa95bd';
+const VERSION = '19.0.0-next.1+sha-c279523';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
