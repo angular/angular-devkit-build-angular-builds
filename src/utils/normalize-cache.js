2ad33fe45ef61371cd6f817d9ed14baaaf4e60ef
Angular Builds <circleci@angular.dev>
2024-09-09 17:26:18 +0000

    f8b2203 docs: elaborate on debugging jasmine node tests

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.3+sha-eb97c43';
+const VERSION = '19.0.0-next.3+sha-f8b2203';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
