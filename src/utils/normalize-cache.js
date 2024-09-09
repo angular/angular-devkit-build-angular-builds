0278ed08110e50a09d1eebb9d66aeb2f37d7cb7c
Angular Builds <circleci@angular.dev>
2024-09-09 17:40:15 +0000

    e12a66a docs(@angular/cli): add playwright to e2e targets

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.3+sha-328ed65';
+const VERSION = '18.2.3+sha-e12a66a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
