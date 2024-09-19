df28f050f49602e5d3740ca62df26e8963b13818
Angular Builds <circleci@angular.dev>
2024-09-19 19:34:24 +0000

    3b00fc9 feat(@angular/build): introduce `outputMode` option to the application builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.7+sha-8723e0c';
+const VERSION = '19.0.0-next.7+sha-3b00fc9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
