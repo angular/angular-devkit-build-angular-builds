aa5afa818322a6217bdbba0ea05457c14605738d
Angular Builds <circleci@angular.dev>
2024-10-02 13:13:17 +0000

    80686be refactor(@angular/build): avoid hydration warnings when `RenderMode.Client` is set

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.8+sha-41934e7';
+const VERSION = '19.0.0-next.8+sha-80686be';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
