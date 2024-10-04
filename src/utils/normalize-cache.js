2e0d0c9915a112c04ecd625017bb2c00588f09be
Angular Builds <circleci@angular.dev>
2024-10-04 05:35:30 +0000

    6c618d4 fix(@angular/build): update logic to support both internal and external SSR middlewares

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.9+sha-523c2c9';
+const VERSION = '19.0.0-next.9+sha-6c618d4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
