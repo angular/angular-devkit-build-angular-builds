6c3ed8b609796b6ad359a10f6763cac66eb3d050
Angular Builds <circleci@angular.dev>
2024-10-09 13:15:39 +0000

    9445916 fix(@angular/build): `Ctrl + C` not terminating dev-server with SSR

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.7+sha-9b5cfaa';
+const VERSION = '18.2.7+sha-9445916';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
