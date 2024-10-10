3ca9ff87ec76092c7e4ca73535120884f872389e
Angular Builds <circleci@angular.dev>
2024-10-10 11:17:00 +0000

    ac8f81e refactor(@angular/build): reduce inline style language value per call usage

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.10+sha-c48d694';
+const VERSION = '19.0.0-next.10+sha-ac8f81e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
