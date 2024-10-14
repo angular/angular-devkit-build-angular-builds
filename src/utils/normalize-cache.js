ca62102626aaf9be9f0a1ccacd4fff997a08e8d4
Angular Builds <circleci@angular.dev>
2024-10-14 16:39:02 +0000

    237f7c5 fix(@schematics/angular): update browserslist config to include last 2 Android major versions

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.8+sha-1e578df';
+const VERSION = '18.2.8+sha-237f7c5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
