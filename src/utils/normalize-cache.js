8dc47e36bbc754aa49e6b959e643dc40f5de48ab
Angular Builds <circleci@angular.dev>
2024-10-15 16:29:20 +0000

    b91c82d fix(@angular/build): avoid race condition in sass importer

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.8+sha-237f7c5';
+const VERSION = '18.2.8+sha-b91c82d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
