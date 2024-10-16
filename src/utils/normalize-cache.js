54131d68d0b671a71dbdc6643fa2af5dfdcfa2e9
Angular Builds <circleci@angular.dev>
2024-10-16 14:03:50 +0000

    13b65df fix(@angular/build): allow direct bundling of TSX files with application builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.10+sha-b6951f4';
+const VERSION = '19.0.0-next.10+sha-13b65df';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
