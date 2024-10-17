ad884866bd725ee9afb2d5b781d364e43149562e
Angular Builds <circleci@angular.dev>
2024-10-17 14:53:29 +0000

    1654acf fix(@angular/build): relax constraints on external stylesheet component id

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.11+sha-1f4428f';
+const VERSION = '19.0.0-next.11+sha-1654acf';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
