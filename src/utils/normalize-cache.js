9767abcfc8158570f286ac884fda8960c552fdf4
Angular Builds <circleci@angular.dev>
2024-10-28 18:01:59 +0000

    dfa3d4c refactor(@angular/ssr): switch from esbuild to rollup for bundling critters/beasties

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.13+sha-e42d841';
+const VERSION = '19.0.0-next.13+sha-dfa3d4c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
