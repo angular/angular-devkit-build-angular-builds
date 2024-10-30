7b412ab5127c66a86b28cc9311bf0e6b17a36773
Angular Builds <circleci@angular.dev>
2024-10-30 23:52:17 +0000

    7a2d0e5 refactor(@angular/build): remove experimental `buildApplication` overload

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.13+sha-9ad8514';
+const VERSION = '19.0.0-next.13+sha-7a2d0e5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
