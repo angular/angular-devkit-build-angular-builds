241bdea9eaf90c5a07ccd810539573b4e11d892c
Angular Builds <circleci@angular.dev>
2024-10-30 14:30:27 +0000

    87ec15b fix(@angular/build): show error message when error stack is undefined

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.10+sha-fee914e';
+const VERSION = '18.2.10+sha-87ec15b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
