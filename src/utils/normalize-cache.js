921bdf629d84e2b0c4de779eb8d438d05d6c283e
Angular Builds <circleci@angular.dev>
2024-10-30 18:31:21 +0000

    361382a build: add return types for component stylesheet bundler

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-next.13+sha-47d9d3b';
+const VERSION = '19.0.0-next.13+sha-361382a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
