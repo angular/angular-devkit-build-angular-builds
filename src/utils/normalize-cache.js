b617de3daee31e2818320f9341e89afde0009d37
Angular Builds <circleci@angular.dev>
2024-10-31 15:59:56 +0000

    4ae1fd2 refactor(@angular/ssr): add route configs to public API

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-rc.0+sha-7502fee';
+const VERSION = '19.0.0-rc.0+sha-4ae1fd2';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
