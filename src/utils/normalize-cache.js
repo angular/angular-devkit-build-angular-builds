3922aa98f1323e657b973515e9c06a1c17eb97ee
Angular Builds <circleci@angular.dev>
2024-11-01 13:35:04 +0000

    a19ca72 refactor(@angular/build): improve typescript bundling context rebuild checking

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-rc.0+sha-26a4769';
+const VERSION = '19.0.0-rc.0+sha-a19ca72';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
