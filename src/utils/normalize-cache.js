8112a78a98fe2c633d5b13072a8e042125727da9
Angular Builds <circleci@angular.dev>
2024-11-04 14:32:16 +0000

    f58e689 test: add `@parcel/watcher` to allowed postinstall checks

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.11+sha-de95545';
+const VERSION = '18.2.11+sha-f58e689';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
