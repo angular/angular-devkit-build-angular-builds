25afbf7e263d1ade749454d263fd58099c8810b2
Angular Builds <circleci@angular.dev>
2024-11-05 14:04:50 +0000

    daea0ab docs: replace `@note` with `@remarks`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.0+sha-4633562';
+const VERSION = '19.1.0-next.0+sha-daea0ab';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
