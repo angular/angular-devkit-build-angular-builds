4c473af55961a49e7a8f755429515a762214c9e4
Angular Builds <circleci@angular.dev>
2024-11-06 15:24:39 +0000

    2ec877d fix(@angular-devkit/build-angular): handle basename collisions

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-rc.0+sha-cf0228b';
+const VERSION = '19.0.0-rc.0+sha-2ec877d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
