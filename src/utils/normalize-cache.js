4f17077ef3338b6b96e95a0ba66be4a3817d68d4
Angular Builds <circleci@angular.dev>
2024-11-13 17:13:24 +0000

    c3925ed fix(@angular/cli): support default options for multiselect list x-prompt

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.11+sha-c8bee84';
+const VERSION = '18.2.11+sha-c3925ed';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
