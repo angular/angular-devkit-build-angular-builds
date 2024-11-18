93b9e9a15dd30a0a64c6d2f609d8ec5bd5243dcf
Angular Builds <dev-infra@angular.dev>
2024-11-18 16:53:18 +0000

    ea5ae68 fix(@angular-devkit/build-angular): bring back style tags in browser builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0-rc.2+sha-9e6ab1b';
+const VERSION = '19.0.0-rc.2+sha-ea5ae68';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
