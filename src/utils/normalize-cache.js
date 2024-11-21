1a177e8851ec738d381ffa36be1451b3c27f688c
Angular Builds <dev-infra@angular.dev>
2024-11-21 13:43:12 +0000

    919f5b9 refactor(@angular/build): add initial infrastructure to support inline template HMR

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0+sha-8bd2b26';
+const VERSION = '19.0.0+sha-919f5b9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
