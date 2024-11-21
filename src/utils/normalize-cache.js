901fdf7de6ddddf0fe266329740c9a0258979b4a
Angular Builds <dev-infra@angular.dev>
2024-11-21 07:25:57 +0000

    8bd2b26 fix(@angular/ssr): handle baseHref that start with `./`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0+sha-74461da';
+const VERSION = '19.0.0+sha-8bd2b26';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
