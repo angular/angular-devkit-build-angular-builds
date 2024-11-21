c51e650bacae1e6919c739a44e17ae1e5f19b861
Angular Builds <dev-infra@angular.dev>
2024-11-21 14:02:31 +0000

    6a87b1e refactor(@angular/build): adjust code to propagate errors from Piscina

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.0+sha-919f5b9';
+const VERSION = '19.0.0+sha-6a87b1e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
