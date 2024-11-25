7d6bc1084a992481413c10f550876c0bb7ba5160
Angular Builds <dev-infra@angular.dev>
2024-11-25 20:22:42 +0000

    c497749 fix(@angular/build): prevent errors with parameterized routes when `getPrerenderParams` is undefined

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.1+sha-d8cbf3b';
+const VERSION = '19.0.1+sha-c497749';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
