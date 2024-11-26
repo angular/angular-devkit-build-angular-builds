8edcef1fd1e3f87bdaf7485b6e64737b542151a8
Angular Builds <dev-infra@angular.dev>
2024-11-26 14:04:40 +0000

    ab4e77c fix(@angular/build): allow .json file replacements with application builds

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.2+sha-d261246';
+const VERSION = '19.0.2+sha-ab4e77c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
