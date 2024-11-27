b0abca6a741445bd8f26334318ccb59e30213c5e
Angular Builds <dev-infra@angular.dev>
2024-11-27 14:05:29 +0000

    4ef45ec fix(@angular/cli): correctly select package versions in descending order during `ng add`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.0+sha-3a22d44';
+const VERSION = '19.1.0-next.0+sha-4ef45ec';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
