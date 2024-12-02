51a997ffe24414ea1f42c1578f7369ca52337b00
Angular Builds <dev-infra@angular.dev>
2024-12-02 14:36:10 +0000

    c5a83cc build: update dependency tar to v7

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.2+sha-28a51cc';
+const VERSION = '19.0.2+sha-c5a83cc';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
