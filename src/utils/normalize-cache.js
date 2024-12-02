45dd3a8ff04ba8a2996d96fa26d3ab222ae5b60f
Angular Builds <dev-infra@angular.dev>
2024-12-02 14:45:26 +0000

    5880a02 fix(@angular/ssr): correctly handle serving of prerendered i18n pages

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.2+sha-c5a83cc';
+const VERSION = '19.0.2+sha-5880a02';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
