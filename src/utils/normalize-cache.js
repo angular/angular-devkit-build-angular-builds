410112603b748ad1469ab104a8b36176395689ba
Angular Builds <dev-infra@angular.dev>
2024-12-02 09:23:55 +0000

    28a51cc fix(@schematics/angular): add required type to `CanDeactivate` guard (#29004)

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.2+sha-3d5f41f';
+const VERSION = '19.0.2+sha-28a51cc';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
