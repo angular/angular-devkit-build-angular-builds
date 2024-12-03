8d3c2e4af4d10fdea48e6b0dfd2e257459ffc153
Angular Builds <dev-infra@angular.dev>
2024-12-03 07:39:40 +0000

    be21acc ci: run workflows for all releasable branches

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.2+sha-5880a02';
+const VERSION = '19.0.2+sha-be21acc';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
