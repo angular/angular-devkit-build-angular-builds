4c6c838e570bac25df2a1e0efb3f8d081d3a4f48
Angular Builds <dev-infra@angular.dev>
2024-12-05 19:23:20 +0000

    f46d0b1 refactor(@angular/build): move `getDepOptimizationConfig` into utils file

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.4+sha-6cfbb88';
+const VERSION = '19.0.4+sha-f46d0b1';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
