181a6567e83d3424dc7b371604d389e2ada91a33
Angular Builds <circleci@angular.dev>
2024-12-05 15:25:10 +0000

    0434eff ci: update dev-infra sha to use latest Bazel keys

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.12+sha-53b9e35';
+const VERSION = '18.2.12+sha-0434eff';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
