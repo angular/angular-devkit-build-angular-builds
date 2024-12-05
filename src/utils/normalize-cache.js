e191a6cab3a2cdaa083e3d72335eda5c3a635215
Angular Builds <dev-infra@angular.dev>
2024-12-05 08:44:23 +0000

    75998eb perf(@angular/build): reuse TS package.json cache when rebuilding

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.0+sha-15a48a3';
+const VERSION = '19.1.0-next.0+sha-75998eb';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
