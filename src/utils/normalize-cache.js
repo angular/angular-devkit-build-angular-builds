968d3b827316538a18a22d2d1e78d28ed68fea76
Angular Builds <dev-infra@angular.dev>
2024-12-11 13:57:00 +0000

    544d139 refactor(@angular/ssr): move ignored messages as a global

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.4+sha-251bd9f';
+const VERSION = '19.0.4+sha-544d139';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
