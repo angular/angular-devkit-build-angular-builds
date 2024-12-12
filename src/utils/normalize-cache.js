a2010f379131ddcc5a370c97aecf92c236651047
Angular Builds <dev-infra@angular.dev>
2024-12-12 18:20:39 +0000

    c66782b refactor(@angular/build): only load chunk optimizer if enabled

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.1+sha-7fda6b5';
+const VERSION = '19.1.0-next.1+sha-c66782b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
