488e29d265c8da7dd5ef162cd70b697b4177d5fa
Angular Builds <dev-infra@angular.dev>
2024-12-13 20:16:46 +0000

    a9a3470 fix(@angular-devkit/build-angular): jasmine.clock with app builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.1+sha-8d35b9b';
+const VERSION = '19.1.0-next.1+sha-a9a3470';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
