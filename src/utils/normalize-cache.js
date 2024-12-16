35197dbf6bd2b5cdcf82be4db88f134824769d6d
Angular Builds <dev-infra@angular.dev>
2024-12-16 18:20:43 +0000

    59985c2 ci: ignore changes to `.yarn/releases`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.1+sha-e4ff5c9';
+const VERSION = '19.1.0-next.1+sha-59985c2';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
