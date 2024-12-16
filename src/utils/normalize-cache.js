78c64388dca133ecdf50b2a5907b14f28df91f99
Angular Builds <dev-infra@angular.dev>
2024-12-16 17:40:36 +0000

    e4ff5c9 ci: sync package locks information for aspect with renovate

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.1+sha-b1500d3';
+const VERSION = '19.1.0-next.1+sha-e4ff5c9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
