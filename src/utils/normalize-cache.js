50a3f31385dbf3c9fe3d2a0c33f94276c95a7be3
Angular Builds <dev-infra@angular.dev>
2024-12-16 14:03:04 +0000

    78c41f6 refactor(@angular/ssr): add timeout to prevent 'adev' hanging during route extraction

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.1+sha-1d6d264';
+const VERSION = '19.1.0-next.1+sha-78c41f6';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
