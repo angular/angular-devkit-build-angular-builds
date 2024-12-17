6cd67908345febe63dcc3a3616673912f51807ac
Angular Builds <dev-infra@angular.dev>
2024-12-17 20:38:48 +0000

    0789a9e fix(@angular/ssr): ensure correct `Location` header for redirects behind a proxy

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.5+sha-5ebeb37';
+const VERSION = '19.0.5+sha-0789a9e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
