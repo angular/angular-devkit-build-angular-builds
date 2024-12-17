a219693816192ca3d1cd20ce01ec681ac8718bb7
Angular Builds <dev-infra@angular.dev>
2024-12-17 19:32:56 +0000

    5ebeb37 refactor(@angular/ssr): mark `LINK_LOAD_SCRIPT_CONTENT` as `@PURE`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.5+sha-df36a86';
+const VERSION = '19.0.5+sha-5ebeb37';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
