b304e3c65e853cf796cb6d42ce66875cef7873fb
Angular Builds <dev-infra@angular.dev>
2024-12-20 12:31:47 +0000

    b43c648 fix(@angular/build): mitigate JS transformer worker execArgv errors

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.6+sha-78d84ea';
+const VERSION = '19.0.6+sha-b43c648';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
