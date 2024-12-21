4e3172647f9f9f259ab5a328ee130f0ebe2392f6
Angular Builds <dev-infra@angular.dev>
2024-12-21 13:48:14 +0000

    5a93866 build: migrate `@ngtools/webpack` to `ts_project`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.6+sha-073b55c';
+const VERSION = '19.0.6+sha-5a93866';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
