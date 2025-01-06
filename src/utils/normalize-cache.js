155ee00da47c1bb0b7f52001213217699820f849
Angular Builds <dev-infra@angular.dev>
2025-01-06 17:28:50 +0000

    78237a9 build: migrate `@angular-devkit/schematics` to `ts_project`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.6+sha-95c22ae';
+const VERSION = '19.0.6+sha-78237a9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
