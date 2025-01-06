bcc1712c9c0cbd99ac028c13fd99bfe277aa0943
Angular Builds <dev-infra@angular.dev>
2025-01-06 14:42:32 +0000

    ddae37f refactor(@angular/build): normalize source path for windows compatibility

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-next.2+sha-aa6bf07';
+const VERSION = '19.1.0-next.2+sha-ddae37f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
