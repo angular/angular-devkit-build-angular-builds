352c1e0eca3143ef10d0a43f92a1eefb16a4f66f
Angular Builds <dev-infra@angular.dev>
2025-01-07 12:26:05 +0000

    c4691fb test(@angular/build): separate application and dev-server integration test targets

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.6+sha-739fd8d';
+const VERSION = '19.0.6+sha-c4691fb';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
