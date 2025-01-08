f36e8a3b6993e1075c1af83c86a62a656a0e31b7
Angular Builds <dev-infra@angular.dev>
2025-01-08 14:57:55 +0000

    1f2481a fix(@angular/build): pass `define` option defined in application builder to Vite prebundling

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.6+sha-48988ea';
+const VERSION = '19.0.6+sha-1f2481a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
