9641d911992e93785a8cfb4729e6c476710b23f4
Angular Builds <dev-infra@angular.dev>
2025-01-08 13:21:50 +0000

    0b7962d build: do not include tsconfig files in npm archives

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.6+sha-1bf59cf';
+const VERSION = '19.0.6+sha-0b7962d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
