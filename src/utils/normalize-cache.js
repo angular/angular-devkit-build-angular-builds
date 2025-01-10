2a36a07acf696b0bd18163570f6ddedb886a28d1
Angular Builds <dev-infra@angular.dev>
2025-01-10 16:41:31 +0000

    e41d8ff build: update `_exec` to handle timeouts gracefully when `waitForMatch` does not match within 60 seconds.

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.7+sha-d0ad668';
+const VERSION = '19.0.7+sha-e41d8ff';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
