d2d9edbfaa22ae594444d63b7203c90f2eead0c8
Angular Builds <dev-infra@angular.dev>
2025-01-14 08:53:21 +0000

    f55d777 fix(@angular-devkit/schematics): update `Rule` type to support returning a `Promise` of `Tree`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.7+sha-217939a';
+const VERSION = '19.0.7+sha-f55d777';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
