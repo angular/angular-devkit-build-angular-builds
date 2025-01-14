8f12a0e7103c37cd1c3e8162e1a3fef5707be782
Angular Builds <dev-infra@angular.dev>
2025-01-14 15:47:01 +0000

    e7201a8 refactor(@ngtools/webpack): add return types to functions

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.0.7+sha-f55d777';
+const VERSION = '19.0.7+sha-e7201a8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
