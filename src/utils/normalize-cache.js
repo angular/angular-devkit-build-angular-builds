90a62e1005ef8805f6b4be08173eeb979fd7fc35
Angular Builds <dev-infra@angular.dev>
2025-01-15 22:55:33 +0000

    0a7415e docs: update release doc with script to remove prerelease versions

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.0+sha-ac37218';
+const VERSION = '19.2.0-next.0+sha-0a7415e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
