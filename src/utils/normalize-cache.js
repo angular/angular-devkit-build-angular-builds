af048a09141055aee3b9ff534da0df2341a4695e
Angular Builds <dev-infra@angular.dev>
2025-01-15 16:44:36 +0000

    92c7674 build: migrate @angular-devkit/schematics-cli to npm_package

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.0+sha-e5368b8';
+const VERSION = '19.2.0-next.0+sha-92c7674';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
