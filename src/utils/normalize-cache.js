e4f5e7cc80d2f4fba05b63f5e7bfdb82acb75871
Angular Builds <dev-infra@angular.dev>
2025-01-15 19:05:48 +0000

    a092dfd build: migrate `@angular-devkit/architect` jasmine test to native `rules_js`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.0-rc.0+sha-84bbfa5';
+const VERSION = '19.1.0-rc.0+sha-a092dfd';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
