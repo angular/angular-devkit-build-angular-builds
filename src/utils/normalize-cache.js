9964962f7b87c3c90fc948377cd7812d52b7af9c
Angular Builds <dev-infra@angular.dev>
2025-01-15 14:14:08 +0000

    e5368b8 refactor(@ngtools/webpack): add types to exported module variables

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.0+sha-23ebfd9';
+const VERSION = '19.2.0-next.0+sha-e5368b8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
