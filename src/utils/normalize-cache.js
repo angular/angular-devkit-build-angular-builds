18047a82bf25f7c3e320f3b0a77f3cc69d7a775c
Angular Builds <dev-infra@angular.dev>
2025-01-16 16:52:31 +0000

    c37b59e build: exclude `pnpm` and `dependenciesMeta` fields from published NPM packages

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.0+sha-3861b88';
+const VERSION = '19.2.0-next.0+sha-c37b59e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
