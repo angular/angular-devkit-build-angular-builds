09eeb33240795e9726460cbcc89abd0347e0efc0
Angular Builds <dev-infra@angular.dev>
2025-01-20 15:25:54 +0000

    bcc5fab fix(@angular/ssr): prevent route matcher error when SSR routing is not used

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.3+sha-4595ca6';
+const VERSION = '19.1.3+sha-bcc5fab';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
