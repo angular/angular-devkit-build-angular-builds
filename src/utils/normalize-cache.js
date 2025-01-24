b51a39db1a5c9cdc4c4c109e920c78b1b3d5478f
Angular Builds <dev-infra@angular.dev>
2025-01-24 12:29:09 +0000

    bd9d379 fix(@angular/build): disable TypeScript `removeComments` option

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.4+sha-94643d5';
+const VERSION = '19.1.4+sha-bd9d379';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
