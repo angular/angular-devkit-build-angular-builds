36eafe302f3e8440758eba50f8961db5cc4dcac3
Angular Builds <dev-infra@angular.dev>
2025-01-29 19:09:58 +0000

    249f9dc refactor(@angular/build): remove unused import

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.1+sha-319fbb6';
+const VERSION = '19.2.0-next.1+sha-249f9dc';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
