3e20556e68ebf11e56480fc9b19e45f1a0f33b28
Angular Builds <circleci@angular.dev>
2025-01-29 12:36:35 +0000

    9d34d28 fix(@angular-devkit/build-angular): remove unused `vite` dependency

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.13+sha-d619f3d';
+const VERSION = '18.2.13+sha-9d34d28';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
