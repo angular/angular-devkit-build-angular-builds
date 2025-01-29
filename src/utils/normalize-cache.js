60a65e6c290c7c0fec07a7e0a661ec355a8b08b4
Angular Builds <dev-infra@angular.dev>
2025-01-29 16:40:55 +0000

    2b9c00f fix(@angular/build): prevent fallback to serving main.js for unknown requests

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.5+sha-5a6f853';
+const VERSION = '19.1.5+sha-2b9c00f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
