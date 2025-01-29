ba911fd5a894ed794e18957313f80e9065661a0f
Angular Builds <dev-infra@angular.dev>
2025-01-29 09:24:28 +0000

    539336e build: migrate remaining `@angular/ssr` tests to `rules_js`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.0+sha-832bfff';
+const VERSION = '19.2.0-next.0+sha-539336e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
