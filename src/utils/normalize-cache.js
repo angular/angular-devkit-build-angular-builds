b6c1213b3bd1bc6cc0926d14abfeb76091f81ccb
Angular Builds <dev-infra@angular.dev>
2025-01-31 17:39:59 +0000

    5bfbf2e ci: disable evalutations that arecausing codeql timeouts

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.5+sha-45abd15';
+const VERSION = '19.1.5+sha-5bfbf2e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
