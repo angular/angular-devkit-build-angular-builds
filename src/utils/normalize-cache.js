5d12325153d367df2076557ee9ebf3d1fc1b3f08
Angular Builds <dev-infra@angular.dev>
2025-02-03 23:30:22 +0000

    25dbe7c refactor(@angular/ssr): simplify preload append logic in metadata

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.1+sha-c48a2da';
+const VERSION = '19.2.0-next.1+sha-25dbe7c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
