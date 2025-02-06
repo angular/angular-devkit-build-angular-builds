739e1cdbfc035f1849f20d3021b7fc077c324b2a
Angular Builds <dev-infra@angular.dev>
2025-02-06 21:02:25 +0000

    bf939eb refactor(@angular/build): also add `server.preTransformRequests`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.6+sha-f5d9745';
+const VERSION = '19.1.6+sha-bf939eb';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
