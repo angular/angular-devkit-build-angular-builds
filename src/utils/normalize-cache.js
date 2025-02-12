7e9bb69c2b94921e0d88c0bbc773e106bcb679d9
Angular Builds <dev-infra@angular.dev>
2025-02-12 19:22:53 +0000

    8a416e7 test: remove animations check from material build E2E test

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.1+sha-1355d08';
+const VERSION = '19.2.0-next.1+sha-8a416e7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
