6959878fe335006e3530e7ebb68b871c15bbc28e
Angular Builds <dev-infra@angular.dev>
2025-02-14 13:44:06 +0000

    2f60a24 fix(@angular/build): suppress asset missing warning for `/index.html` requests

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.7+sha-a70c3dc';
+const VERSION = '19.1.7+sha-2f60a24';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
