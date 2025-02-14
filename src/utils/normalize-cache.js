926327e05256f77414fd34efd08d808fa2ffbf26
Angular Builds <dev-infra@angular.dev>
2025-02-14 15:42:25 +0000

    c0c1670 fix(@angular-devkit/build-angular): pass missing options to Karma esbuild builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.2+sha-486d477';
+const VERSION = '19.2.0-next.2+sha-c0c1670';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
