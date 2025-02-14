3eeaad81e66fe13a97c3286ab70b48042aa34e83
Angular Builds <dev-infra@angular.dev>
2025-02-14 14:25:40 +0000

    486d477 build: modify Alan in the mailmap file

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.2+sha-65c99b8';
+const VERSION = '19.2.0-next.2+sha-486d477';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
