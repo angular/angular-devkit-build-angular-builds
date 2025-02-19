e7cef0e33cdd6936c9495e390758df8114bd555c
Angular Builds <dev-infra@angular.dev>
2025-02-19 18:23:59 +0000

    c2cbc0f build: add @types/node to assorted locations where node types are being relied on and currently obtained transitively.

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.1.7+sha-b8f7952';
+const VERSION = '19.1.7+sha-c2cbc0f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
