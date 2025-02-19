cdd6e3d5ef1a36827e692e7737e507a88ccc292b
Angular Builds <dev-infra@angular.dev>
2025-02-19 22:44:20 +0000

    4255779 release: switch the next branch to v20.0.0-next.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0-next.2+sha-8f73dad';
+const VERSION = '20.0.0-next.0+sha-4255779';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
