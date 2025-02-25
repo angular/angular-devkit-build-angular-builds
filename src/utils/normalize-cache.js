21ff20f620b8969782854f2b513b7f28ba158a1d
Angular Builds <dev-infra@angular.dev>
2025-02-25 18:29:21 +0000

    eb058d0 fix(@angular/build): always disable JSON stats with dev-server

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.0+sha-2f8e7db';
+const VERSION = '20.0.0-next.0+sha-eb058d0';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
