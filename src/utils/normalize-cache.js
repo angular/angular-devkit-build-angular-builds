48e8595149cc8091f3e34d19498d54ac113a9f06
Angular Builds <dev-infra@angular.dev>
2025-02-28 16:15:13 +0000

    25ea098 fix(@angular/build): handle undefined `less` stylesheet sourcemap values

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.0+sha-44b06f4';
+const VERSION = '20.0.0-next.0+sha-25ea098';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
