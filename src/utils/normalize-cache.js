9ea5c0d1104eba3526a06b537f4f71547fc01315
Angular Builds <dev-infra@angular.dev>
2025-03-04 09:03:21 +0000

    c073309 fix(@angular/build): allow component HMR with a service worker

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.0+sha-4e4fb8a';
+const VERSION = '19.2.0+sha-c073309';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
