94ad119a2fbb31a7ce8681d165425894b593ed61
Angular Builds <dev-infra@angular.dev>
2025-03-05 18:28:38 +0000

    78297ee fix(@angular/build): provide `extract-i18n` does not respect `i18nDuplicateTranslation` config value (#29179)

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.1+sha-e40d5fe';
+const VERSION = '19.2.1+sha-78297ee';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
