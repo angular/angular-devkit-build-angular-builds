6a570bb0584047ae954ebd6aad66400124556882
Angular Builds <dev-infra@angular.dev>
2025-03-06 18:22:15 +0000

    4575265 fix(@angular/build): exclude all entrypoints of a library from prebundling

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.1+sha-78297ee';
+const VERSION = '19.2.1+sha-4575265';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
