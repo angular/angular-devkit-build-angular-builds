32fd5bd37b04b33bc0b56979bf1bd8a3ad678064
Angular Builds <dev-infra@angular.dev>
2025-03-07 08:01:04 +0000

    956e806 build: update devinfra digest to 60ad70b

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.0+sha-f0dd60b';
+const VERSION = '20.0.0-next.0+sha-956e806';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
