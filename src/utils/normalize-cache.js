9566ab10d869beac6393cc68596b3814455a02a1
Angular Builds <dev-infra@angular.dev>
2025-03-11 14:35:00 +0000

    e6e8ce9 fix(@angular/ssr): prevent stream draining if `write` does not return a boolean

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.1+sha-83fcffb';
+const VERSION = '19.2.1+sha-e6e8ce9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
