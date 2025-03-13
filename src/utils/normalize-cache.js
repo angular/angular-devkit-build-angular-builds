0e79243859efb2bab2c8a976d4276baf8475756c
Angular Builds <dev-infra@angular.dev>
2025-03-13 17:59:58 +0000

    f780e8b fix(@angular/build): ensure relative karma stack traces for test failures

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.1+sha-18e13e2';
+const VERSION = '20.0.0-next.1+sha-f780e8b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
