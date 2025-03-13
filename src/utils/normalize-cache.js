a11bca935eeec60c3591e08f1b7d548ac37e1c91
Angular Builds <dev-infra@angular.dev>
2025-03-13 08:17:42 +0000

    54c561a build: avoid WSL interop flake when preparing Windows CI environment

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.0+sha-2d9735d';
+const VERSION = '20.0.0-next.0+sha-54c561a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
