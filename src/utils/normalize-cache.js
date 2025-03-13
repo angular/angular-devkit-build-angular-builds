f18ca029c0f92f4ebec3eab2198846a183946dfd
Angular Builds <dev-infra@angular.dev>
2025-03-13 13:59:09 +0000

    40b228d docs(@angular-devkit/build-angular): update `forceEsbuild` option description

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.3+sha-e4d1c2a';
+const VERSION = '19.2.3+sha-40b228d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
