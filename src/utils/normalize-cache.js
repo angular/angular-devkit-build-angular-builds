3f91158697fc13f4b79bd9a4df2360ce276ea34c
Angular Builds <dev-infra@angular.dev>
2025-03-13 23:45:12 +0000

    318c164 refactor(@angular/build): define an internal ngHmrMode value

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.1+sha-f780e8b';
+const VERSION = '20.0.0-next.1+sha-318c164';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
