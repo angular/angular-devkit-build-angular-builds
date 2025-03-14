b36434e9e7ff64bb3c88c1236b2ec6c5ca1562cb
Angular Builds <dev-infra@angular.dev>
2025-03-14 17:24:40 +0000

    29a58e5 build: move build-angular/build-webpack specific dependencies out of root

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.1+sha-f4be831';
+const VERSION = '20.0.0-next.1+sha-29a58e5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
