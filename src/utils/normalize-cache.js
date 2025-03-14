a7aaa96d53ff2caa10a123b4f6fb71b8d0593407
Angular Builds <dev-infra@angular.dev>
2025-03-14 15:09:38 +0000

    f4be831 feat(@angular/build): Support Sass package importers

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.1+sha-27fe5da';
+const VERSION = '20.0.0-next.1+sha-f4be831';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
