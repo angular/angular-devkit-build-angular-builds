54974e1be9181835491957e189fbf058ea719ae5
Angular Builds <dev-infra@angular.dev>
2025-03-17 21:25:07 +0000

    e07491e build: move devkit core specific dependencies out of root

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.1+sha-e774476';
+const VERSION = '20.0.0-next.1+sha-e07491e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
