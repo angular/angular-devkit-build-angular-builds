5d4493f22b966392fd37aa2b17d9fac3cad5dea4
Angular Builds <dev-infra@angular.dev>
2025-03-21 14:08:15 +0000

    32b1dcd fix(@angular/build): handle undefined `getOrCreateAngularServerApp` during error compilation

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.4+sha-20455e2';
+const VERSION = '19.2.4+sha-32b1dcd';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
