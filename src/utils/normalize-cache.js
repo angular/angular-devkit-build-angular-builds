b691dcd7c46a63112a11a34df1b2389f3611851f
Angular Builds <dev-infra@angular.dev>
2025-03-25 08:09:51 +0000

    7552a9f fix(@angular/build): normalize karma asset paths before lookup

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.4+sha-32b1dcd';
+const VERSION = '19.2.4+sha-7552a9f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
