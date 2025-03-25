7af2c8b20ea2710cc7c3592a0c46f8dd3533554d
Angular Builds <dev-infra@angular.dev>
2025-03-25 18:46:19 +0000

    5c9b371 refactor(@angular/build): use newer Node.js `cp` API for asset copying

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.2+sha-c1de633';
+const VERSION = '20.0.0-next.2+sha-5c9b371';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
