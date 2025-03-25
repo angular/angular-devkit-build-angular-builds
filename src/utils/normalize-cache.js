a8d2de546bdd3b6ba5743ce3667bcf2215ef123e
Angular Builds <circleci@angular.dev>
2025-03-25 11:22:34 +0000

    4267a80 fix(@angular-devkit/build-angular): remove `@vitejs/plugin-basic-ssl` from dependencies

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.15+sha-18ea0db';
+const VERSION = '18.2.15+sha-4267a80';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
