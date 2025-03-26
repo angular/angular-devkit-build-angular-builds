a14347c2a96ff0f78c393635e0b6b1d5843d30ee
Angular Builds <dev-infra@angular.dev>
2025-03-26 08:49:58 +0000

    1eb5b43 fix(@angular/build): update vite to 6.2.3

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.4+sha-7552a9f';
+const VERSION = '19.2.4+sha-1eb5b43';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
