b6bde01e2f3cc18f5b1896ca8e3c21c0a50bee00
Angular Builds <dev-infra@angular.dev>
2025-03-26 08:37:07 +0000

    2b73b72 test: ensure atime and mtime are rounded consistently in tests

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.2+sha-1f62b28';
+const VERSION = '20.0.0-next.2+sha-2b73b72';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
