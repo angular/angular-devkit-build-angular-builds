d02dff46b3a65b33473364a5370a8b1360391fee
Angular Builds <dev-infra@angular.dev>
2025-03-27 18:58:29 +0000

    cac7f3c ci: update bazel RBE setup on CI and use trusted build configuration for upstream CI runs

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.5+sha-d556d32';
+const VERSION = '19.2.5+sha-cac7f3c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
