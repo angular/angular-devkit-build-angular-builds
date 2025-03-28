6fb3bd14c3931fc4565487cb5cc603994eb6d51c
Angular Builds <dev-infra@angular.dev>
2025-03-28 08:26:54 +0000

    2703f0a refactor(@angular/build): remove `Component HMR has been enabled` warning

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.3+sha-41d2d17';
+const VERSION = '20.0.0-next.3+sha-2703f0a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
