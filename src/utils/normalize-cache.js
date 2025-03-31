bea186e4db871348dc43862200cba336cde2e88f
Angular Builds <dev-infra@angular.dev>
2025-03-31 10:58:19 +0000

    8a89438 fix(@angular/build): correctly handle  `false` value in server option

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.3+sha-088a4cb';
+const VERSION = '20.0.0-next.3+sha-8a89438';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
