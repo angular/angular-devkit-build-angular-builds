ff676d3722cd492d7c933b8f35246f646bc9194f
Angular Builds <dev-infra@angular.dev>
2025-04-01 13:09:40 +0000

    e5aec56 fix(@angular-devkit/schematics): properly resolve relative schematics when executed from a nested directory

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.5+sha-76cfd36';
+const VERSION = '19.2.5+sha-e5aec56';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
