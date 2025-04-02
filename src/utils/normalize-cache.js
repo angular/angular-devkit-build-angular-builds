1e0aed0e8ba9808f8ce7a38edfa21a87762bf178
Angular Builds <circleci@angular.dev>
2025-04-02 13:36:43 +0000

    247ceff fix(@angular/build): update vite to 5.4.16 due to a security issues

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.16+sha-e4b6363';
+const VERSION = '18.2.16+sha-247ceff';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
