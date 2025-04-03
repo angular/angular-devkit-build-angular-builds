269188681cd279b0b79cfcf9ab586fcd3440c7f6
Angular Builds <dev-infra@angular.dev>
2025-04-03 10:52:47 +0000

    322f6ec refactor(@schematics/angular): use `generateFromFiles` helper in more cases

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.4+sha-bfdd8d5';
+const VERSION = '20.0.0-next.4+sha-322f6ec';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
