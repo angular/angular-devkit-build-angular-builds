bbf311cd90cb917799bf7916a82fe0f3c232aa4e
Angular Builds <dev-infra@angular.dev>
2025-04-08 13:12:32 +0000

    0253bc9 test: update E2E tests to account for the `.html` template extension

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.4+sha-070d60f';
+const VERSION = '20.0.0-next.4+sha-0253bc9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
