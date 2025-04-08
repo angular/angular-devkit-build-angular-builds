541f804ee43c609bafd73a880405e390a8867150
Angular Builds <dev-infra@angular.dev>
2025-04-08 11:02:30 +0000

    f4021e6 build: update `beasties` to `0.3.2`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.6+sha-74cd4ed';
+const VERSION = '19.2.6+sha-f4021e6';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
