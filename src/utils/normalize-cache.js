16891f6c1b94e526e29bf6ecb39acc1a2f0cdb20
Angular Builds <dev-infra@angular.dev>
2025-04-10 16:45:53 +0000

    a297c41 fix(@angular/build): properly resolve transitive external dependencies in vite-dev-server

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.7+sha-e0b1bed';
+const VERSION = '19.2.7+sha-a297c41';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
