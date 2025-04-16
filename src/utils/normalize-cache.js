27ef809bdd0d87f54ea410adcf8a160e211d0985
Angular Builds <dev-infra@angular.dev>
2025-04-16 15:26:54 +0000

    cc5229a fix(@angular/build): pass `preserveSymlinks` option to Karma esbuild builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.7+sha-03a420b';
+const VERSION = '19.2.8+sha-cc5229a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
