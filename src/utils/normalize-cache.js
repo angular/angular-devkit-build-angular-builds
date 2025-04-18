0d9c00e50d57c7932f32e80ff54aba14d687ed88
Angular Builds <circleci@angular.dev>
2025-04-18 10:57:27 +0000

    01cc617 fix(@angular-devkit/build-angular): update http-proxy-middleware to v3.0.5

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.18+sha-2b1dc21';
+const VERSION = '18.2.18+sha-01cc617';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
