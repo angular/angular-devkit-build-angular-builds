9805a2a886f4b8de43be626d0196def181676225
Angular Builds <dev-infra@angular.dev>
2025-04-22 09:44:07 +0000

    ad77837 docs: update release docs to describe how to upgrade the browser policy

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.6+sha-c706d50';
+const VERSION = '20.0.0-next.6+sha-ad77837';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
