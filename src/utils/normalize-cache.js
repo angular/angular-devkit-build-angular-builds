a8147ac0c2fe8c4e255e143d2f02c14173fb9aaa
Angular Builds <dev-infra@angular.dev>
2025-04-23 07:49:26 +0000

    8bfcae4 fix(@angular/ssr): support `getPrerenderParams` for wildcard routes

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.6+sha-e03f2b8';
+const VERSION = '20.0.0-next.6+sha-8bfcae4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
