2555f6c5c5149df1b90f599398da9e02021ab2e0
Angular Builds <dev-infra@angular.dev>
2025-04-25 16:54:58 +0000

    2a25652 build: remove legacy rnjs setup

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.9+sha-2a7cb6e';
+const VERSION = '19.2.9+sha-2a25652';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
