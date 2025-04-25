c8b846da00337ddd8f09c33fa7e2141b02abd806
Angular Builds <dev-infra@angular.dev>
2025-04-25 08:23:08 +0000

    05485ed fix(@angular/build): ensure `com.chrome.devtools.json` is consistently served after initial run

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.8+sha-2ee9042';
+const VERSION = '20.0.0-next.8+sha-05485ed';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
