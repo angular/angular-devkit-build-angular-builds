1af88680d7c43f163a69c3fe2f307b1e6fa85e94
Angular Builds <dev-infra@angular.dev>
2025-04-30 23:59:35 +0000

    8c5e5e6 refactor(@angular-devkit/schematics): add explicit return types to functions

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-next.9+sha-02cb7fc';
+const VERSION = '20.0.0-next.9+sha-8c5e5e6';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
