958e812462f01c88cecac2f5186e982a21dc33a3
Angular Builds <dev-infra@angular.dev>
2025-05-04 22:41:15 +0000

    009fc37 fix(@angular/build): avoid internal karma request cache for assets

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.10+sha-3e3abf4';
+const VERSION = '19.2.10+sha-009fc37';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
