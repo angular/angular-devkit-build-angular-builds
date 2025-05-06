c8f51d3deb5201cf0ad9212f76411fdbc27ba9c2
Angular Builds <dev-infra@angular.dev>
2025-05-06 18:24:00 +0000

    ed92841 fix(@angular/cli): add Node.js 24 as supported version

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0-next.0+sha-e29c6a2';
+const VERSION = '20.1.0-next.0+sha-ed92841';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
