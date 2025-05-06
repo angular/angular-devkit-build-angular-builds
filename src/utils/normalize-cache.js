bfb54cb9b8d74de08a4af51965afc8e3c6139f05
Angular Builds <dev-infra@angular.dev>
2025-05-06 11:19:56 +0000

    9eaf344 fix(@angular-devkit/build-angular): correctly set i18n subPath in webpack browser builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.10+sha-cba66a8';
+const VERSION = '19.2.10+sha-9eaf344';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
