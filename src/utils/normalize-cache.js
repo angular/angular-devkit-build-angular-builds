8ffdf9e86fa96288cd0a7cfc83bb68e5f9ae8a58
Angular Builds <dev-infra@angular.dev>
2025-05-14 19:48:18 +0000

    d2bfc6b fix(@angular/build): revert setup unit-test polyfills before TestBed init

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.1+sha-c2030ed';
+const VERSION = '20.0.0-rc.1+sha-d2bfc6b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
