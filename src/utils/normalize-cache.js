b3f42739b22c8a3ca0b607d40aa591baed3eb82b
Angular Builds <dev-infra@angular.dev>
2025-05-14 13:37:18 +0000

    255bc55 build: update dependency terser to v5.39.1

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.0+sha-0d40cde';
+const VERSION = '20.0.0-rc.0+sha-255bc55';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
