378acbff5b61b0500f53b61e25fd986033ad1c49
Angular Builds <dev-infra@angular.dev>
2025-05-15 13:27:55 +0000

    9c29cf7 refactor: remove unused function

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.2+sha-a5caafd';
+const VERSION = '20.0.0-rc.2+sha-9c29cf7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
