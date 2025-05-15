86ea7dd3636f76927c1607da66b23cf733a7b71f
Angular Builds <dev-infra@angular.dev>
2025-05-15 21:24:22 +0000

    059c10e fix(@angular/ssr): SSR should work without `@angular/router`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.2+sha-eee816f';
+const VERSION = '20.0.0-rc.2+sha-059c10e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
