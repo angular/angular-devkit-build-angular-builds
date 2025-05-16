ef354c50c7bc4133e5861f8c9b9249eba295ba80
Angular Builds <dev-infra@angular.dev>
2025-05-16 17:02:38 +0000

    b264517 build: update dependency aspect_rules_ts to v3.6.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.2+sha-4b57934';
+const VERSION = '20.0.0-rc.2+sha-b264517';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
