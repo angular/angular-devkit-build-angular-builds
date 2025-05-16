dfcc7d7a94d556c6979f68e01ff340241d7d9b70
Angular Builds <dev-infra@angular.dev>
2025-05-16 02:13:26 +0000

    b155ba1 fix(@angular/build): enable unit-test builder watch outside CI

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.2+sha-059c10e';
+const VERSION = '20.0.0-rc.2+sha-b155ba1';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
