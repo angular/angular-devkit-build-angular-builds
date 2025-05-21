f58679a54704378cda938c3d148a3eed14f28a75
Angular Builds <dev-infra@angular.dev>
2025-05-21 12:29:01 +0000

    e50d03d build: update dependency watchpack to v2.4.4

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0-next.0+sha-a9021ae';
+const VERSION = '20.1.0-next.0+sha-e50d03d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
