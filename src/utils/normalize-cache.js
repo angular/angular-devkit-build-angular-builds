70c9238fe0914a90b465504f6aa612ad42825c73
Angular Builds <dev-infra@angular.dev>
2025-05-22 11:31:48 +0000

    fdbafa5 refactor(@angular/cli): adjust type usage for eventual ESM building

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.0-rc.3+sha-4c0d873';
+const VERSION = '20.0.0-rc.3+sha-fdbafa5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
