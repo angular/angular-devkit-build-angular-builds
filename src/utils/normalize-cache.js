e4737f3c467d0f25cb550c0caabf7f21e8b3f0e7
Angular Builds <dev-infra@angular.dev>
2025-05-23 07:32:58 +0000

    cf4e676 build: correctly replace `BUILD_SCM_HASH-PLACEHOLDER` with `BUILD_SCM_ABBREV_HASH`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.13+sha-33aee37';
+const VERSION = '19.2.13+sha-cf4e676';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
