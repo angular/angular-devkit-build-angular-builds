5e03177767e024f48599abe2e79f3d42e07f0172
Angular Builds <dev-infra@angular.dev>
2025-05-29 18:39:44 +0000

    664e6c7 fix(@schematics/angular): only overwrite JSON file if actually changed

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0-next.0+sha-c7e73fa';
+const VERSION = '20.1.0-next.0+sha-664e6c7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
