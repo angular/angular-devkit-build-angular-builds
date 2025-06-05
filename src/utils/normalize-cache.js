9d8ad77257fc9c386d72f72b72d9ab4715d01e50
Angular Builds <dev-infra@angular.dev>
2025-06-05 16:25:45 +0000

    1db861c build: improve globbing of jasmine test files

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.1+sha-cb258a3';
+const VERSION = '20.0.1+sha-1db861c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
