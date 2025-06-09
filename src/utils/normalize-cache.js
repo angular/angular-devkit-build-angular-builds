06c84fed82f7930ad1d78c48a5b0971d6e1cd5c9
Angular Builds <dev-infra@angular.dev>
2025-06-09 14:05:21 +0000

    631ac7e ci: use new pull-request-labeling action

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.1+sha-fa25385';
+const VERSION = '20.0.1+sha-631ac7e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
