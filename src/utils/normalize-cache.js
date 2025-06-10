cac0f2cc55ffd1fb554902ea52a8ce6d4fb60efe
Angular Builds <circleci@angular.dev>
2025-06-10 22:42:25 +0000

    f048078 build: update dependency webpack-dev-server to v5.2.2

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.19+sha-cfb0a4f';
+const VERSION = '18.2.19+sha-f048078';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
