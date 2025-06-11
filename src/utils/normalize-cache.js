af0e45aea404d461bec88ef4770a03189b92900e
Angular Builds <dev-infra@angular.dev>
2025-06-11 18:14:59 +0000

    0371f9a test(@angular/ssr): mark jasmine tests as flaky

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.2+sha-a61528b';
+const VERSION = '20.0.2+sha-0371f9a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
