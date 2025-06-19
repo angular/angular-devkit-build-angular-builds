ae30045b60bd5bf0c8dfd2f14b7f82b78fb5d520
Angular Builds <dev-infra@angular.dev>
2025-06-19 07:16:38 +0000

    d45d2a4 refactor(@schematics/angular): add link to ai best practicies

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0-next.2+sha-22815c3';
+const VERSION = '20.1.0-next.2+sha-d45d2a4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
