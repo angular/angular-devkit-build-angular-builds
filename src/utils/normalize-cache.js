2c811a08a4bc70be344268785f76c245b5cf4299
Angular Builds <dev-infra@angular.dev>
2025-06-20 13:52:56 +0000

    caf70d8 docs: update component schematic option descriptions

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0-next.2+sha-8c95e8e';
+const VERSION = '20.1.0-next.2+sha-caf70d8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
