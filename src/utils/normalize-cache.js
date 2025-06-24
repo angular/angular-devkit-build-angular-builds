0d6b08df1a5c4bc019c27016bedb860707367438
Angular Builds <dev-infra@angular.dev>
2025-06-24 14:34:25 +0000

    1c5bd2e fix(@angular/ssr): ensure correct referer header handling in web request conversion

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0-next.2+sha-a418a1d';
+const VERSION = '20.1.0-next.2+sha-1c5bd2e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
