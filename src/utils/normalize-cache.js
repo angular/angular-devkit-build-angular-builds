b650e717dc4f64b4faab767d44240fa16eb478b4
Angular Builds <dev-infra@angular.dev>
2025-07-02 21:38:19 +0000

    41e28ff test: raise timeout of `//packages/ngtools/webpack:test`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.5+sha-f5c2d67';
+const VERSION = '20.0.5+sha-41e28ff';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
