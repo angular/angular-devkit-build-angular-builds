7388315650444ac9f7300e44edca400315621885
Angular Builds <dev-infra@angular.dev>
2025-07-03 09:08:50 +0000

    1cde40e fix(@angular/build): correctly remap Angular diagnostics

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-next.0+sha-d96a093';
+const VERSION = '20.2.0-next.0+sha-1cde40e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
