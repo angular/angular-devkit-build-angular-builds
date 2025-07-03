f28bef1446504a6bc7ae6cfbb23f803c9e91fd88
Angular Builds <dev-infra@angular.dev>
2025-07-03 13:24:23 +0000

    5542445 fix(@schematics/angular): remove constructor from service template

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.0.5+sha-1a2da16';
+const VERSION = '20.0.5+sha-5542445';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
