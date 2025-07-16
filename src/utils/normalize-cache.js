64a763b37df56985a01e7135012a523276f2c392
Angular Builds <dev-infra@angular.dev>
2025-07-16 22:12:29 +0000

    9dfdaf5 test: use valid theme for material schematic

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.1+sha-15c0148';
+const VERSION = '20.1.1+sha-9dfdaf5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
