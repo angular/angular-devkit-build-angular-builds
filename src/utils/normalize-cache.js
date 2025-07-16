d741ca2e0ab11b8f08c73b88e210534f4adbfe42
Angular Builds <dev-infra@angular.dev>
2025-07-16 18:56:56 +0000

    0925ebd refactor(@schematics/angular): fix layout of pill button

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.0+sha-3ccd1e8';
+const VERSION = '20.1.0+sha-0925ebd';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
