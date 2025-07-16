c5375d139d88d34302e0c619e1d123500d59159e
Angular Builds <dev-infra@angular.dev>
2025-07-16 15:04:50 +0000

    3faa3b4 refactor(@angular/build): provide private factory API for internal compilation infrastructure

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-next.0+sha-0ee847c';
+const VERSION = '20.2.0-next.0+sha-3faa3b4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
