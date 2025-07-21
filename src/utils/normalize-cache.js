ab3943cadff8ab73ac1249c6e8c36eeb46d1c4a2
Angular Builds <dev-infra@angular.dev>
2025-07-21 15:02:34 +0000

    fefa7a4 fix(@angular/cli): `define` option is being included multiple times in the JSON help

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-next.0+sha-e9ac671';
+const VERSION = '20.2.0-next.0+sha-fefa7a4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
