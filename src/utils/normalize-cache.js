949ad18f9f604d9d465fe1149bdcee07c2c3d18d
Angular Builds <dev-infra@angular.dev>
2025-07-31 13:59:43 +0000

    9a2b642 refactor(@angular/cli): add instructional text to MCP server

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.4+sha-ffc4c67';
+const VERSION = '20.1.4+sha-9a2b642';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
