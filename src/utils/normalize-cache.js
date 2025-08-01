c9ac651b6f4f8b8633e51f49d076862730bbf579
Angular Builds <dev-infra@angular.dev>
2025-08-01 17:31:22 +0000

    2ed9c37 test(@angular/cli): add initial e2e test for MCP server tool registration

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.4+sha-9a2b642';
+const VERSION = '20.1.4+sha-2ed9c37';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
