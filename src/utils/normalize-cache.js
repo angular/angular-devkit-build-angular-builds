f18cd9c25d3d2ae59191c9145751fe226e465483
Angular Builds <dev-infra@angular.dev>
2025-08-04 14:14:09 +0000

    b6b2578 refactor(@angular/cli): centralize environment variable parsing

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-next.2+sha-6f936fb';
+const VERSION = '20.2.0-next.2+sha-b6b2578';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
