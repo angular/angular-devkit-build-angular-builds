44c7ae5f439e93ecad6acc2db237cbe1d6307d83
Angular Builds <dev-infra@angular.dev>
2025-08-12 15:43:28 +0000

    584bc1d fix(@schematics/angular): add extra prettier config

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.5+sha-0d050f3';
+const VERSION = '20.1.5+sha-584bc1d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
