5cec4cc0ad46a3f7c9d4f2f51fdb0b916b02b4a4
Angular Builds <dev-infra@angular.dev>
2025-08-12 08:12:11 +0000

    0d050f3 build: update to latest version of dev-infra

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.5+sha-2c0e973';
+const VERSION = '20.1.5+sha-0d050f3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
