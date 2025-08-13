524d44b00c6ac0381c545c742a748dd733ec009e
Angular Builds <dev-infra@angular.dev>
2025-08-13 10:35:05 +0000

    721b690 refactor(@angular/cli): use .html as template extension for mcp modernize tool

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-next.3+sha-0e97b79';
+const VERSION = '20.2.0-next.3+sha-721b690';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
