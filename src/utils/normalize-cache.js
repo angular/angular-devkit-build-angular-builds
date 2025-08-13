4296a716de7a85d2aa0adb6ebe6c87f942c4d4f7
Angular Builds <dev-infra@angular.dev>
2025-08-13 12:20:09 +0000

    d655e43 refactor(@schematics/angular): remove duplicate prompt definition.

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-next.3+sha-e46d9c5';
+const VERSION = '20.2.0-next.3+sha-d655e43';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
