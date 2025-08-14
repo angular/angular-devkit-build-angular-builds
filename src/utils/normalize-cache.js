b920b97b0cb26d6550961c54722aa7ce3991f7a7
Angular Builds <dev-infra@angular.dev>
2025-08-14 08:25:28 +0000

    b105b4d build: update dependency @inquirer/prompts to v7.8.2

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-rc.0+sha-8ba6b0b';
+const VERSION = '20.2.0-rc.0+sha-b105b4d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
