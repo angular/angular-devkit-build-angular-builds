2e975c08d64c8976dadc557c3655744c66021dce
Angular Builds <dev-infra@angular.dev>
2025-08-14 13:55:27 +0000

    8b824bd build: add retry-delay for `accept4 failed 110`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.1.6+sha-d67c260';
+const VERSION = '20.1.6+sha-8b824bd';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
