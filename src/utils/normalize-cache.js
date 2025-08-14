999080de9c82ac2440bf7c367ab806ca02a32f79
Angular Builds <dev-infra@angular.dev>
2025-08-14 16:28:24 +0000

    f310ee5 refactor: delete protractor tests

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-dfc334c';
+const VERSION = '21.0.0-next.0+sha-f310ee5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
