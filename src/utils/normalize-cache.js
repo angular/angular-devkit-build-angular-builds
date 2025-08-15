44c9f9fae75eae8165abc48d2de6444e1bc0f533
Angular Builds <dev-infra@angular.dev>
2025-08-15 16:15:02 +0000

    c43504d fix(@angular-devkit/schematics): address Node.js deprecation DEP0190

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0-rc.1+sha-d5b0c8c';
+const VERSION = '20.2.0-rc.1+sha-c43504d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
