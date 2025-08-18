5b72e4dcc7b0fd3e9f24e6b9ab838710fe0cbd36
Angular Builds <dev-infra@angular.dev>
2025-08-18 16:39:58 +0000

    c3789e4 fix(@angular/cli): apply default to array types

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-6c64560';
+const VERSION = '21.0.0-next.0+sha-c3789e4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
