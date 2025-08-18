c8be029a6926c657d823e2530b986c0051b38a8a
Angular Builds <dev-infra@angular.dev>
2025-08-18 06:40:28 +0000

    f8ad0f9 refactor(@schematics/angular): remove unneeded `getFileContent` unit-test helper

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-aace49e';
+const VERSION = '21.0.0-next.0+sha-f8ad0f9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
