ec7a1be231de8e67413bd58b6aed9b4084c7ef24
Angular Builds <dev-infra@angular.dev>
2025-08-19 15:57:00 +0000

    754c7b4 build: update bazel module lock file

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-f54cdff-with-local-changes';
+const VERSION = '21.0.0-next.0+sha-754c7b4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
