365c0582e9066ff49ba58b139c4a1d232c37213e
Angular Builds <dev-infra@angular.dev>
2025-08-21 16:00:23 +0000

    ffff9ea refactor(@angular/cli): allow first-party schematics to override encapsulation

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0+sha-ec75be0';
+const VERSION = '20.2.0+sha-ffff9ea';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
