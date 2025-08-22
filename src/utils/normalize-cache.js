7d019b917856f936f6daba2d1c8867389322db5d
Angular Builds <dev-infra@angular.dev>
2025-08-22 12:29:47 +0000

    b554bd7 fix(@angular/build): add temporary directory cleanup for Vitest executor

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-290ac55';
+const VERSION = '21.0.0-next.0+sha-b554bd7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
