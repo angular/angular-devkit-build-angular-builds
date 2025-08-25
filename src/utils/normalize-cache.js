1ddc0af3dbd75c45bcb5fdfbbb81d5fde38a1baa
Angular Builds <dev-infra@angular.dev>
2025-08-25 16:23:13 +0000

    5179d80 ci: Disable PNPM cache on Windows

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0+sha-3071317';
+const VERSION = '20.2.0+sha-5179d80';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
