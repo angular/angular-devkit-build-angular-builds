1086e15a1bb91f0e14c2fb7df279f1c75f7984e4
Angular Builds <dev-infra@angular.dev>
2025-08-25 16:44:51 +0000

    264848a build: update cross-repo angular dependencies

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-b5ff82b';
+const VERSION = '21.0.0-next.0+sha-264848a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
