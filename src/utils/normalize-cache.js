e1a4d155a78ed475d5bf4405811aa11abb91814c
Angular Builds <dev-infra@angular.dev>
2025-08-26 18:47:47 +0000

    db775e1 build: update actions/download-artifact action to v5

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.0+sha-9a12ea7';
+const VERSION = '21.0.0-next.0+sha-db775e1';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
