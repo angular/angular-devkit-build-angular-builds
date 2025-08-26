8ed0d957c35dc161ab0d49ff835bf8ed7dde00d3
Angular Builds <dev-infra@angular.dev>
2025-08-26 13:52:44 +0000

    ecc0198 ci: removing WSL for a simpler, more performant approach

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.0+sha-5c76d4c';
+const VERSION = '20.2.0+sha-ecc0198';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
