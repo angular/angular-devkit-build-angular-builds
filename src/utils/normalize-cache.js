f97038c25cf048130558f6cb62a712c3b5d9e280
Angular Builds <dev-infra@angular.dev>
2025-08-28 13:51:29 +0000

    7e8ae01 refactor(@angular/cli): improve logging with `ng add` for packages without schematics

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.1+sha-14c0a9b';
+const VERSION = '21.0.0-next.1+sha-7e8ae01';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
