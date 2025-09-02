30925da2ca90a7dac0853a40bc6ca3589bf25495
Angular Builds <dev-infra@angular.dev>
2025-09-02 17:54:08 +0000

    5c2abff fix(@angular/build): avoid extra tick in SSR dev-server builds

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.1+sha-0eaec23';
+const VERSION = '20.2.1+sha-5c2abff';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
