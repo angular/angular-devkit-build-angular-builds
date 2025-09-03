7e1355652a94c4fdeb31c44c0402de29c4fec104
Angular Builds <dev-infra@angular.dev>
2025-09-03 13:19:05 +0000

    2736599 fix(@angular/cli): set process title when running architect commands

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.1+sha-f3c8268';
+const VERSION = '20.2.1+sha-2736599';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
