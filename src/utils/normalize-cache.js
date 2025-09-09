78b74b0ff4f60d1ebafa736da0db9fd9903a4fdd
Angular Builds <dev-infra@angular.dev>
2025-09-09 05:37:04 +0000

    e6a3b55 test(@angular/build): remove color codes before assert in vitest snapshot E2E

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.2+sha-2037b91';
+const VERSION = '21.0.0-next.2+sha-e6a3b55';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
