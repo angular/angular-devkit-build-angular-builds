0b6d0fee510e890b9dc4c7b19e435ad43f4cf75f
Angular Builds <dev-infra@angular.dev>
2025-09-09 11:29:15 +0000

    a48db0a refactor(@angular/cli): exclude Cnpm from LOCKFILE_NAMES type

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.2.2+sha-e54fc81';
+const VERSION = '20.2.2+sha-a48db0a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
