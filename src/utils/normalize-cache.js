2b0fc975f56bce107da12715b75149dac7bf9c65
Angular Builds <dev-infra@angular.dev>
2025-09-10 12:30:56 +0000

    ef20a27 fix(@schematics/angular): align labels in ai-config schema

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.0-next.0+sha-e21bd5c';
+const VERSION = '20.3.0-next.0+sha-ef20a27';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
