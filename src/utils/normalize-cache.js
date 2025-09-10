f9efc111aa9749cb75e9fc82c333ec6add499770
Angular Builds <dev-infra@angular.dev>
2025-09-10 08:49:15 +0000

    1ee9ce3 feat(@angular/cli): promote MCP `find_examples` tool to a stable tool

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.2+sha-a60de9b';
+const VERSION = '21.0.0-next.2+sha-1ee9ce3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
