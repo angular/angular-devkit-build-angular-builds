3fb514563cece4588958a88dafcae8b523ba90a5
Angular Builds <dev-infra@angular.dev>
2025-09-11 13:06:48 +0000

    be60be4 fix(@angular/build): add timestamp to bundle generation log

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.0+sha-4e51032';
+const VERSION = '20.3.0+sha-be60be4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
