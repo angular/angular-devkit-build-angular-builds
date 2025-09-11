7912f81235e46c0fe09feb858caf1528fbe1d6b7
Angular Builds <circleci@angular.dev>
2025-09-11 14:05:05 +0000

    875aa6c ci: increase resources for CI jobs

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '18.2.21+sha-0e3ba96';
+const VERSION = '18.2.21+sha-875aa6c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
