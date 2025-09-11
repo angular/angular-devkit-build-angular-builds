e5101bce9c05eb0c5296818c7d44cacaf0008afd
Angular Builds <dev-infra@angular.dev>
2025-09-11 12:25:28 +0000

    4e51032 refactor(@angular/cli): Add instructions to component zoneless migration to retain NgZone.run

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.0+sha-db1c025';
+const VERSION = '20.3.0+sha-4e51032';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
