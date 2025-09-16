a67dcf0361efd2020009f2c8485f16b15afad79f
Angular Builds <dev-infra@angular.dev>
2025-09-16 11:58:48 +0000

    d0631c5 Revert "fix(@angular/build): preserve names in esbuild for improved debugging in dev mode"

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.3+sha-a908bf3';
+const VERSION = '21.0.0-next.3+sha-d0631c5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
