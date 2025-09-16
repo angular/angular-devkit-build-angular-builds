deb5528abb0362a4602e63755736043a8f3ed767
Angular Builds <dev-infra@angular.dev>
2025-09-16 11:20:09 +0000

    c0b00d7 feat(@angular/build): add reporter output file option for unit-test

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.3+sha-66dd6dd';
+const VERSION = '21.0.0-next.3+sha-c0b00d7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
