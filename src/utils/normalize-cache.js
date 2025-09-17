0fe8a271a2c212264d0f7383438addead040ffa8
Angular Builds <dev-infra@angular.dev>
2025-09-17 13:17:48 +0000

    34b88c8 build: enable minimumReleaseAge to mitigate dependency chain attacks

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.1+sha-5b804a5';
+const VERSION = '20.3.1+sha-34b88c8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
