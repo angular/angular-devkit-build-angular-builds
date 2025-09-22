5c566b5f91004b344cceda756c7149249b97767f
Angular Builds <dev-infra@angular.dev>
2025-09-22 14:50:01 +0000

    aa5581b build: update rolldown to v1.0.0-beta.38

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.2+sha-d7f392b';
+const VERSION = '20.3.2+sha-aa5581b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
