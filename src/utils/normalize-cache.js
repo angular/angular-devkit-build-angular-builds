06f7c0447597e68a509409ff97fe2f35da2e23d7
Angular Builds <dev-infra@angular.dev>
2025-09-22 16:33:20 +0000

    2b0f6d6 refactor(@angular/ssr): disable streaming when rendering SSG page

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.4+sha-cb8980a';
+const VERSION = '21.0.0-next.4+sha-2b0f6d6';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
