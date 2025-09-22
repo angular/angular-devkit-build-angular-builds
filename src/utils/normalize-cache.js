e38be41981952b33ceae57cc4666e2c614790ba6
Angular Builds <dev-infra@angular.dev>
2025-09-22 11:23:35 +0000

    049eb55 refactor(@angular/cli): add version support to doc-search MCP tool

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.4+sha-54a2ca3';
+const VERSION = '21.0.0-next.4+sha-049eb55';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
