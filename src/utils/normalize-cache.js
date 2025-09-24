ae5be3ebab328fddcbb18df66bd957b768b50a65
Angular Builds <dev-infra@angular.dev>
2025-09-24 14:16:13 +0000

    3af4dcb fix(@schematics/angular): add `__screenshots__/` to `.gitignore`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.4+sha-7a8c946';
+const VERSION = '21.0.0-next.4+sha-3af4dcb';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
