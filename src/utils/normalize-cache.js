595bb3e48689a5e309a2ccc0b635f33b429d85b8
Angular Builds <dev-infra@angular.dev>
2025-09-29 15:35:16 +0000

    c94bf7f fix(@schematics/angular): Out of the box support for PM2

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.3+sha-0b5481e';
+const VERSION = '20.3.3+sha-c94bf7f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
