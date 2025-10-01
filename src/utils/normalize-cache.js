fd4fbb81827c7b1dd7f6cfa2a78a09d7c8560ad2
Angular Builds <dev-infra@angular.dev>
2025-10-01 13:08:24 +0000

    c14ebe7 build: update ossf/scorecard-action action to v2.4.3

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.3+sha-ee2e4ee';
+const VERSION = '20.3.3+sha-c14ebe7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
