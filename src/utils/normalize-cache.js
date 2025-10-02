d339c43f6e0f1a648f5d4219c163fd6b4de882a7
Angular Builds <dev-infra@angular.dev>
2025-10-02 00:20:37 +0000

    7761180 refactor(@angular/build): default tsConfig option in unit-test builder

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.5+sha-e155aac';
+const VERSION = '21.0.0-next.5+sha-7761180';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
