60034d89c0b22df08077841c89b1e7ec6c1d8819
Angular Builds <dev-infra@angular.dev>
2025-10-02 19:56:24 +0000

    d0787c1 fix(@angular/build): provide default excludes for vitest coverage

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.5+sha-f263306';
+const VERSION = '21.0.0-next.5+sha-d0787c1';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
