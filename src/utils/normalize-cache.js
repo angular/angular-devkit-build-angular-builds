202e1535a36f01e6486ed3b99b19c8de55e3d772
Angular Builds <dev-infra@angular.dev>
2025-10-06 14:56:45 +0000

    7362199 fix(@angular/build): normalize paths for Vitest runner output files

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.6+sha-e4fa5f3';
+const VERSION = '21.0.0-next.6+sha-7362199';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
