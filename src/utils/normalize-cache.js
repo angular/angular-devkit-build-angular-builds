5d858ab50d3f443828a54b15eb21032b5bd0ddd5
Angular Builds <dev-infra@angular.dev>
2025-10-14 13:54:04 +0000

    ad113c7 refactor: remove standalone flag

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.7+sha-f8c0744';
+const VERSION = '21.0.0-next.7+sha-ad113c7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
