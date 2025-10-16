96034d196761f42f10ed15a9448244865dfc945b
Angular Builds <dev-infra@angular.dev>
2025-10-16 13:32:00 +0000

    85c18b4 fix(@angular/ssr): correctly handle routes with matrix parameters

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.8+sha-ad80848';
+const VERSION = '21.0.0-next.8+sha-85c18b4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
