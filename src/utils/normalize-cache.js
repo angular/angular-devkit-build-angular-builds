343a52522039eb8e8e00b9a2265f5f88cd6a3148
Angular Builds <dev-infra@angular.dev>
2025-10-17 17:38:23 +0000

    8c6b3e7 refactor(@angular/ssr): simplify redirect URL determination

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.6+sha-8cdda11';
+const VERSION = '20.3.6+sha-8c6b3e7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
