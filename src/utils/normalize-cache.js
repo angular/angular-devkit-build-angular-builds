1ab9a301fbf6a3858c2f991f07ea154fe30d63ae
Angular Builds <dev-infra@angular.dev>
2025-10-20 13:26:23 +0000

    7fd2b7b build: update baseline date to October 2025

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.8+sha-341057f';
+const VERSION = '21.0.0-next.8+sha-7fd2b7b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
