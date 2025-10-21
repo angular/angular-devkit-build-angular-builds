e097e23279fc60c31456cceb619ecc266b065488
Angular Builds <dev-infra@angular.dev>
2025-10-21 12:54:03 +0000

    a25aa52 build: remove unused npm dependency

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.8+sha-f125c6f';
+const VERSION = '21.0.0-next.8+sha-a25aa52';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
