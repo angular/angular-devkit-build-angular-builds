3e8112e0ce68bcb3666b8d4aa8ffca76054bd1dd
Angular Builds <dev-infra@angular.dev>
2025-10-22 21:45:40 +0000

    512ad28 fix(@schematics/angular): preserve blank lines in jasmine-to-vitest schematic

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-next.8+sha-a660d69';
+const VERSION = '21.0.0-next.8+sha-512ad28';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
