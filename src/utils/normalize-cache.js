68a3b348c10b484a4c2fc93703ff03947eb17766
Angular Builds <dev-infra@angular.dev>
2025-10-23 12:28:16 +0000

    813cba9 fix(@angular-devkit/build-angular): expand jest and jest-environment-jsdom to allow version 30

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.7+sha-7b521dd';
+const VERSION = '20.3.7+sha-813cba9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
