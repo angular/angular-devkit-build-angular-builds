4e1376d35bdb9af6dddbf147e26dc5630facb2d8
Angular Builds <dev-infra@angular.dev>
2025-10-28 11:55:45 +0000

    e6ccc74 refactor(@schematics/angular): add trailing comma to generated services

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.7+sha-2c7581c';
+const VERSION = '20.3.7+sha-e6ccc74';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
