19678084f5205d527a9170c06f328e5d2b3f4627
Angular Builds <dev-infra@angular.dev>
2025-11-04 15:39:43 +0000

    bddf211 docs: clarify `outputMode` description in application schema

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.8+sha-683697e';
+const VERSION = '20.3.8+sha-bddf211';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
