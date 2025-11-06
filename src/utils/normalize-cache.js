15f928511bb66a0fcf4d768e72dc60a917be88df
Angular Builds <dev-infra@angular.dev>
2025-11-06 17:00:37 +0000

    0851d2e fix(@angular/build): show full aggregate errors from vitest

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.1+sha-c4fd220';
+const VERSION = '21.0.0-rc.1+sha-0851d2e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
