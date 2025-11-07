f45ab7ce97037534c7207c75a9b0d9411b55b556
Angular Builds <dev-infra@angular.dev>
2025-11-07 18:07:08 +0000

    6fbd106 build: migrate license file handling to `write_source_file`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.9+sha-7c11e07';
+const VERSION = '20.3.9+sha-6fbd106';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
