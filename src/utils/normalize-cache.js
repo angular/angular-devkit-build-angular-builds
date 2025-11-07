e8c6f3219658e782da1e010dd8a50ff8d73e67b1
Angular Builds <dev-infra@angular.dev>
2025-11-07 10:58:46 +0000

    a8465d4 refactor: fix several typos (#31716)

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.0+sha-c3c9ac5';
+const VERSION = '21.1.0-next.0+sha-a8465d4';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
