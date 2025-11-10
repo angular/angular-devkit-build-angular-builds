486393c8a36b2d699bb529735bd9e4992f7b187e
Angular Builds <dev-infra@angular.dev>
2025-11-10 14:03:58 +0000

    dca04d6 build: update dependency esbuild-wasm to v0.26.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.1+sha-41b1250';
+const VERSION = '21.0.0-rc.1+sha-dca04d6';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
