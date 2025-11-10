4f6f8ab434393d9dc4883abe17b152174b51c8fa
Angular Builds <dev-infra@angular.dev>
2025-11-10 07:32:56 +0000

    c854a71 fix(@schematics/angular): correct `tsconfig.spec.json` include for spec files

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.9+sha-b3908f6';
+const VERSION = '20.3.9+sha-c854a71';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
