0927f6e77c6a3dcc5d3f104cf707ab8bd3b6ff70
Angular Builds <dev-infra@angular.dev>
2025-11-11 16:31:15 +0000

    b65d22e build: update dependency jsdom to v27.1.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.1+sha-ee6338b';
+const VERSION = '21.0.0-rc.1+sha-b65d22e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
