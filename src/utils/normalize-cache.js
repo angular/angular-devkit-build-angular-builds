0b68cd870f458225e919d1796f0130c33d1c85da
Angular Builds <dev-infra@angular.dev>
2025-11-12 16:02:18 +0000

    599b3b8 refactor(@angular/build): improve Vitest configuration merging

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.1+sha-1b6f15c';
+const VERSION = '21.0.0-rc.1+sha-599b3b8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
