860138693f3998347fe93286ff732f0b129e8598
Angular Builds <dev-infra@angular.dev>
2025-11-12 21:20:51 +0000

    72f2a46 refactor(@schematics/angular): rename jasmine-to-vitest schematic

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.0+sha-9c3d5ff';
+const VERSION = '21.1.0-next.0+sha-72f2a46';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
