a7b2a3d77de574c4806e04283415b13188475e75
Angular Builds <dev-infra@angular.dev>
2025-11-13 17:53:14 +0000

    4deac3e fix(@angular/cli): support multi-database search in find_examples MCP tool

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.2+sha-1c3f562';
+const VERSION = '21.0.0-rc.2+sha-4deac3e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
