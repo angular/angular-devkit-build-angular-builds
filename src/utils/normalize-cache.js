bd31f50753561eed62d64a1e0f14e20d430ff5db
Angular Builds <dev-infra@angular.dev>
2025-11-14 19:17:38 +0000

    0830f4f fix(@angular/build): ensure TestBed cleanup hooks are always registered

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.3+sha-8d63d59';
+const VERSION = '21.0.0-rc.3+sha-0830f4f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
