f81d3bcacfb8cb504319c89b937577d897fbe7c8
Angular Builds <dev-infra@angular.dev>
2025-11-14 20:46:03 +0000

    7217478 ci: bump specs for e2e-package-managers

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.0+sha-ca55398';
+const VERSION = '21.1.0-next.0+sha-7217478';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
