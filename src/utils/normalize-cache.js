69251c1508ed5717c1844356a4b7e96114def850
Angular Builds <dev-infra@angular.dev>
2025-11-14 00:37:00 +0000

    8d63d59 test(@angular/build): add test with 'vitest' import in browser mode

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.2+sha-8a53e2d';
+const VERSION = '21.0.0-rc.3+sha-8d63d59';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
