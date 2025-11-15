6de31491c9b5ebba703836df9596d5b7ccc13c45
Angular Builds <dev-infra@angular.dev>
2025-11-15 13:32:26 +0000

    bb29254 test(@angular/build): add e2e for a larger project with Vitest

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.4+sha-49b65ab';
+const VERSION = '21.0.0-rc.4+sha-bb29254';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
