fe50a8fa0b718697e81ea28bcc8ce6ddd9227fb6
Angular Builds <dev-infra@angular.dev>
2025-11-17 18:47:15 +0000

    fa5c923 fix(@angular/build): prioritize string type for runnerConfig schema

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0-rc.4+sha-e8feba9';
+const VERSION = '21.0.0-rc.4+sha-fa5c923';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
