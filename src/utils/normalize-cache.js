0f50444d0ba83483fb14d4f4b63214ebec282480
Angular Builds <dev-infra@angular.dev>
2025-11-17 17:30:35 +0000

    70e67fe fix(@schematics/angular): add missing typeSeparator to main.ts.template file

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.0+sha-102ca8b';
+const VERSION = '21.1.0-next.0+sha-70e67fe';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
