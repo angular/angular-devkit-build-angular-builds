5714d69c6275f397c007cb4944fb88bd0e58ec20
Angular Builds <dev-infra@angular.dev>
2025-11-18 18:31:55 +0000

    a8bd8f7 fix(@schematics/angular): flag '--file-name-style-guide=2016' - wrong import in main.ts

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.0+sha-8bd7f28';
+const VERSION = '21.1.0-next.0+sha-a8bd8f7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
