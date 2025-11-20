2bc295ed74732d6ef5d05bf9088348a4c579c2b8
Angular Builds <dev-infra@angular.dev>
2025-11-20 15:11:46 +0000

    6c2c10b refactor: modernize array and string last element access using `.at(-1)`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0+sha-cdb607a';
+const VERSION = '21.0.0+sha-6c2c10b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
