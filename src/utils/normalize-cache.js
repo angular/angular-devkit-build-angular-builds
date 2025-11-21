d9eb985108732c815490c1cbd1010573b56e38f2
Angular Builds <dev-infra@angular.dev>
2025-11-21 08:15:48 +0000

    3cac018 fix(@angular/ssr): prevent redirect loop with encoded query parameters

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0+sha-572e559';
+const VERSION = '21.0.0+sha-3cac018';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
