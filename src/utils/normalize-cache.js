9b05bcb683621dc8cd4f56775ea06037151f0f1b
Angular Builds <dev-infra@angular.dev>
2025-11-24 15:08:03 +0000

    2f58705 fix(@schematics/angular): add missing imports for lifecycle hooks in jasmine-vitest migration

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0+sha-afac5e7';
+const VERSION = '21.0.0+sha-2f58705';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
