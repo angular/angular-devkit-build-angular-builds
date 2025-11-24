0f2ec0d8a9402a0ce9578d8fde1e82f5c8faada6
Angular Builds <dev-infra@angular.dev>
2025-11-24 16:17:03 +0000

    7222b54 docs: mention Vitest instead of Karma in README template of workspace files

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0+sha-16d898e';
+const VERSION = '21.0.0+sha-7222b54';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
