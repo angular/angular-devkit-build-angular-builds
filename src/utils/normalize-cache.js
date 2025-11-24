7a33f634d8321c3a64f5327ffb6f61ece95165f6
Angular Builds <dev-infra@angular.dev>
2025-11-24 15:32:58 +0000

    16d898e fix(@schematics/angular): fix migration of `jasmine.clock().mockDate()`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.0+sha-2f58705';
+const VERSION = '21.0.0+sha-16d898e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
