44148e9975c904a456fb584446207d720e13f143
Angular Builds <dev-infra@angular.dev>
2025-12-03 13:50:11 +0000

    f1a7116 fix(@angular/cli): update `@modelcontextprotocol/sdk` to v1.24.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.1+sha-3d0e5da';
+const VERSION = '21.0.1+sha-f1a7116';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
