02ab52c0477dd12a25ab7d3e9cefae1115ce0b14
Angular Builds <dev-infra@angular.dev>
2025-12-04 08:19:43 +0000

    d8b76e9 fix(@angular/cli): correctly handle yarn classic tag manifest fetching

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.1+sha-ec19c26';
+const VERSION = '21.1.0-next.1+sha-d8b76e9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
