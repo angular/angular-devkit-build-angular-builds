59947a8ec475edca9280578a29fbc8fc2f32419e
Angular Builds <dev-infra@angular.dev>
2025-12-05 01:30:27 +0000

    fa0ddc5 test(@angular/cli): correct unscoped auth E2E test logic

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.1+sha-69b2be8';
+const VERSION = '21.1.0-next.1+sha-fa0ddc5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
