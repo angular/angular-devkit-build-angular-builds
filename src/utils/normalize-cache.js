a8cbea5d141937d409e8b70c580f81100a858e47
Angular Builds <dev-infra@angular.dev>
2025-12-05 14:06:38 +0000

    588f3b8 ci: force ipv4 resolutions first in Node.js

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.1+sha-7038ea1';
+const VERSION = '21.1.0-next.1+sha-588f3b8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
