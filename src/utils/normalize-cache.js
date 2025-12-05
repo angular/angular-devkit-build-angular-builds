48809019247b2a5ee1a163e0631cb1386d4621cb
Angular Builds <dev-infra@angular.dev>
2025-12-05 17:24:36 +0000

    0fc7d57 fix(@angular/build): ensure locale base href retains leading slash (#32040)

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.2+sha-54f1009';
+const VERSION = '21.0.2+sha-0fc7d57';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
