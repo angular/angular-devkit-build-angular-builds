3c83db665634bd3d80fa8fb91fd73a9a6295d946
Angular Builds <dev-infra@angular.dev>
2025-12-08 23:44:39 +0000

    62dc12b test: remove webdriver-manager repo postinstall

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.1+sha-d680b80';
+const VERSION = '21.1.0-next.1+sha-62dc12b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
