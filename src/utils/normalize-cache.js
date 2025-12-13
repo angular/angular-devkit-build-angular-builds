f0edd7d0f7a6a0dd31616074dc20b2ce5caa48b3
Angular Builds <dev-infra@angular.dev>
2025-12-13 00:59:42 +0000

    b671245 fix(@schematics/angular): improve VS Code background compilation start/end detection

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.3+sha-df59b98';
+const VERSION = '21.0.3+sha-b671245';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
