b4abc3b02cb6792874568a8e5a178fb89fc77a1e
Angular Builds <dev-infra@angular.dev>
2025-12-13 00:25:00 +0000

    df59b98 build: update dependency bazel to v8.5.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.3+sha-4dd3c1a';
+const VERSION = '21.0.3+sha-df59b98';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
