546c2f5a7a03d1e2f01d25472400b893f7b44279
Angular Builds <dev-infra@angular.dev>
2025-12-15 12:16:59 +0000

    3a00a55 refactor(@angular-devkit/architect-cli): remove unused progress bar functionality

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.2+sha-52ace04';
+const VERSION = '21.1.0-next.2+sha-3a00a55';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
