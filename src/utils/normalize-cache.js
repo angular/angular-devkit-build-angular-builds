136815ec37edbc54ea8aca80d72b70314234e9f1
Angular Builds <dev-infra@angular.dev>
2025-12-16 19:48:24 +0000

    4b8b7ca fix(@angular/build): ensure tests run when compilation error is resolved

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.3+sha-f86e88c';
+const VERSION = '21.0.3+sha-4b8b7ca';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
