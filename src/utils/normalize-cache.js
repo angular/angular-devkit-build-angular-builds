26913084c336b2a6e54cd311cd48d968d9b2ec57
Angular Builds <dev-infra@angular.dev>
2025-12-17 14:22:30 +0000

    430d189 refactor(@angular-devkit/architect-cli): remove yargs-parser dependency

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.2+sha-60aded8';
+const VERSION = '21.1.0-next.2+sha-430d189';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
