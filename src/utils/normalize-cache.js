65affda515d309cafdc2b82b20a98453b287c938
Angular Builds <dev-infra@angular.dev>
2025-12-17 16:43:51 +0000

    7d3e0ad test: migrate legacy CLI tests to e2e directory

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.2+sha-f39f7ee';
+const VERSION = '21.1.0-next.2+sha-7d3e0ad';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
