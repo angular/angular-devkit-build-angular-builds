bc9dedefdf25dd38f7b87e074607ff89f672cca0
Angular Builds <dev-infra@angular.dev>
2026-01-06 09:18:59 +0000

    5dfc0ee fix(@schematics/angular): update default app component message

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.3+sha-164e7db';
+const VERSION = '21.1.0-next.3+sha-5dfc0ee';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
