32a3ed3728cc6256a2ec47ea2dc6fb8eecad3210
Angular Builds <dev-infra@angular.dev>
2026-01-07 17:31:21 +0000

    32adc3a fix(@angular/build): ensure correct project targeting during Vitest debugging

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.0-next.3+sha-31132bc';
+const VERSION = '21.1.0-next.3+sha-32adc3a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
