45fe56c548d3280a79a34eac6ffd32eb525d1a29
Angular Builds <dev-infra@angular.dev>
2026-01-07 14:07:03 +0000

    8456da3 refactor: update copyright year in license

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.0.4+sha-ac26f4a';
+const VERSION = '21.0.4+sha-8456da3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
