134ca9c79d5b389f55acade09719f50d472ec1d7
Angular Builds <dev-infra@angular.dev>
2026-01-13 14:38:23 +0000

    8ba6967 refactor: improve `findUp` utility correctness and performance

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0-next.0+sha-d9cd609';
+const VERSION = '21.2.0-next.0+sha-8ba6967';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
