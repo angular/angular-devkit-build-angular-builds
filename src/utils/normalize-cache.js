7b9c9e4bea0c9a9fdb5ad32401b9a54f36f4f03b
Angular Builds <dev-infra@angular.dev>
2026-01-21 16:17:55 +0000

    279b1ad release: cut the v20.3.15 release

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.14+sha-795d654';
+const VERSION = '20.3.15+sha-279b1ad';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
