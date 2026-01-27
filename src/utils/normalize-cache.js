ad3b558aca21370d6f357451bc148cf33503f199
Angular Builds <dev-infra@angular.dev>
2026-01-27 18:35:34 +0000

    d66f1fe fix(@angular-devkit/architect): Add boolean type inference for 'true' and 'false' string values in argument parsing

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.1+sha-e974e40';
+const VERSION = '21.1.1+sha-d66f1fe';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
