a1fdb9fa3da69a3af81ddc161dee63f830b2c7f8
Angular Builds <dev-infra@angular.dev>
2026-01-28 13:37:18 +0000

    6f76cd2 build: update pnpm to v10.28.2

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.1.2+sha-6d05d27';
+const VERSION = '21.1.2+sha-6f76cd2';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
