40ce34cb9dea3fc12168da72de32c75040173b73
Angular Builds <dev-infra@angular.dev>
2026-02-09 15:03:56 +0000

    0e6f853 docs: release notes for the v20.3.16 release

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0-next.1+sha-c5c0ac4';
+const VERSION = '21.2.0-next.1+sha-0e6f853';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
