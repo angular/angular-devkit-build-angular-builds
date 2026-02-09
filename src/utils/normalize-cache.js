358e6e26b31fbb31efbb80cba5b03b7c8772725c
Angular Builds <dev-infra@angular.dev>
2026-02-09 13:10:48 +0000

    0f02aca build: update webpack to v5.105.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.15+sha-279b1ad';
+const VERSION = '20.3.15+sha-0f02aca';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
