c15c8718a2cf08632e943205b8f78ac72126428b
Angular Builds <dev-infra@angular.dev>
2026-02-10 13:48:35 +0000

    a391f04 test: execute 'protractor' binary directly in 'browsers' E2E test

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0-next.1+sha-826a994';
+const VERSION = '21.2.0-next.1+sha-a391f04';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
