3e67b0956efc6f1aa0d9c0f0337a9e7fc1a1daaf
Angular Builds <dev-infra@angular.dev>
2026-02-23 16:00:41 +0000

    288e228 fix(@angular/ssr): prevent open redirect via X-Forwarded-Prefix header

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.20+sha-747393c';
+const VERSION = '19.2.20+sha-288e228';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
