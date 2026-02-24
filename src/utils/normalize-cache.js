54968487d15d34b19aa360ec2ceb381351391700
Angular Builds <dev-infra@angular.dev>
2026-02-24 19:19:35 +0000

    1d35e3f build: update dependency @inquirer/confirm to v6

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.0+sha-d0e9e81';
+const VERSION = '22.0.0-next.0+sha-1d35e3f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
