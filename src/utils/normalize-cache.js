1e8cca8012a658f15a8be9db647326f2387228c1
Angular Builds <dev-infra@angular.dev>
2026-02-24 21:10:56 +0000

    dfbbe2d build: update dependency @listr2/prompt-adapter-inquirer to v4

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.0+sha-1d35e3f';
+const VERSION = '22.0.0-next.0+sha-dfbbe2d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
