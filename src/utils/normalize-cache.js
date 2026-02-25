f886c45794f25c57ec147c62f3b07fc27d5b8434
Angular Builds <dev-infra@angular.dev>
2026-02-25 21:42:50 +0000

    481e40d build: bump framework dependencies to latest minor

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0-rc.2+sha-54bd6e3';
+const VERSION = '21.2.0-rc.2+sha-481e40d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
