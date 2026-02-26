b27ef5a49a422ed16e8255956f4cee71c582bc16
Angular Builds <dev-infra@angular.dev>
2026-02-26 12:22:35 +0000

    536a959 build: update rules_browsers digest to 8231142

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0+sha-018d493';
+const VERSION = '21.2.0+sha-536a959';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
