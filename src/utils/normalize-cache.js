ca2ac44e8205cd651d5f4a7cf86d2a1f7e1e3f8f
Angular Builds <dev-infra@angular.dev>
2026-02-26 15:00:17 +0000

    39596d5 fix(@angular-devkit/core): update `ajv` to `8.18.0`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.17+sha-c0d1626';
+const VERSION = '20.3.17+sha-39596d5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
