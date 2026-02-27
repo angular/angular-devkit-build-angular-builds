d0d6613a10836d2030127f4f1aca2a0602161fbb
Angular Builds <dev-infra@angular.dev>
2026-02-27 09:51:37 +0000

    5d32799 build: update repository locations for bazel rules

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0+sha-76f09aa';
+const VERSION = '21.2.0+sha-5d32799';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
