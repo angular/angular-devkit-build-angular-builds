3af0052939ede54a4288de3240102fc5f09c20ef
Angular Builds <dev-infra@angular.dev>
2026-03-03 15:30:50 +0000

    dee3717 fix(@angular/ssr): introduce DI token to signal route discovery process

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.0+sha-3627063';
+const VERSION = '21.2.0+sha-dee3717';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
