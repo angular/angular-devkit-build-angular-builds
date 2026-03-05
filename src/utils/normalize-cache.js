5c25660417a13a770e52d76eb852bfa1698cf504
Angular Builds <dev-infra@angular.dev>
2026-03-05 15:03:33 +0000

    add72f7 ci: remove redundant `actions/checkout` steps from dev-infra workflows

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.1+sha-5d0c64e';
+const VERSION = '21.2.1+sha-add72f7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
