5649c54a3d199bcba120ccb49e53551005a312dd
Angular Builds <dev-infra@angular.dev>
2026-03-09 13:42:53 +0000

    28f4d68 perf(@angular/cli): avoid redundant package version resolution in ng add

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.1+sha-4ca6164';
+const VERSION = '21.2.1+sha-28f4d68';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
