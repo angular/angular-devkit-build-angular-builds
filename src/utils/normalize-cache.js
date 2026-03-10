c7da2b87fdcb45bbe6f2500676c9b0c755210c1c
Angular Builds <dev-infra@angular.dev>
2026-03-10 14:28:36 +0000

    42180e5 refactor: remove `@angular-devkit/core` logger and its dependencies from the e2e runner

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.1+sha-790e18d';
+const VERSION = '21.2.1+sha-42180e5';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
