1fba3ff33aa95b76acdf1acd96e253e1ef569d1e
Angular Builds <dev-infra@angular.dev>
2026-03-17 17:50:32 +0000

    0a2ff0b fix(@angular/ssr): ensure unique values in redirect response Vary header

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.20+sha-1e7d877';
+const VERSION = '20.3.20+sha-0a2ff0b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
