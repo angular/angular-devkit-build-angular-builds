3a17c80103a0fd4073a63311e62e2c53662989f3
Angular Builds <dev-infra@angular.dev>
2026-03-20 15:15:25 +0000

    1a8376b fix(@angular/build): remove default for unit-test coverage option

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.2+sha-2678f5f';
+const VERSION = '22.0.0-next.2+sha-1a8376b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
