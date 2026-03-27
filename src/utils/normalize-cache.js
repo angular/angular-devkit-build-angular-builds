a6f6623a6997c716efec3d8af1ab43892c1600af
Angular Builds <dev-infra@angular.dev>
2026-03-27 09:00:17 +0000

    7d3844a refactor(@angular-devkit/schematics): remove shell usage in git spawn to prevent command injection

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.5+sha-3018612';
+const VERSION = '21.2.5+sha-7d3844a';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
