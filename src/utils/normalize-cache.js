a42dda72a108cbeb18cda54fd899b4c5c196955e
Angular Builds <dev-infra@angular.dev>
2026-03-30 16:48:04 +0000

    8f954e3 build: safeguard terminal styling in devkit admin script

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.5+sha-57e956c';
+const VERSION = '21.2.5+sha-8f954e3';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
