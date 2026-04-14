78c5de23398b991e09cc0a1e41574c636ecf8253
Angular Builds <dev-infra@angular.dev>
2026-04-14 22:34:45 +0000

    2d53fec fix(@angular/build): update esbuild to `0.28.0`

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.24+sha-a7625d1';
+const VERSION = '19.2.24+sha-2d53fec';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
