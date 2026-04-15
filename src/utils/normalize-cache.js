652a56909640336a241e8579e241bda8818de750
Angular Builds <dev-infra@angular.dev>
2026-04-15 13:25:05 +0000

    e91280c fix(@angular/cli): dynamically resolve project Angular CLI executable inside MCP tools

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.7+sha-6df2b14';
+const VERSION = '21.2.7+sha-e91280c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
