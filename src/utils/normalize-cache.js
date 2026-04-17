8e406cf7aeae16d6fc80e4279bbdae6c3231512c
Angular Builds <dev-infra@angular.dev>
2026-04-17 11:45:51 +0000

    0787786 refactor: remove debugger statement

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.7+sha-30e4e9a';
+const VERSION = '21.2.7+sha-0787786';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
