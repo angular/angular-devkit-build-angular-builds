4f0d56039d87010e412e3ed97f0a9b447c9b810c
Angular Builds <dev-infra@angular.dev>
2026-04-27 15:49:03 +0000

    7cea988 fix(@angular/cli): introduce initial package manager workspace awareness

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.8+sha-94023f6';
+const VERSION = '21.2.8+sha-7cea988';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
