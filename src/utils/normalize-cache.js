7385c9db0a20dedef27142e356f46edc2caf4def
Angular Builds <dev-infra@angular.dev>
2026-04-27 17:10:43 +0000

    7b5a86c refactor: remove unused fetchPackageMetadata and fetchPackageManifest utilities

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.6+sha-1b900f0';
+const VERSION = '22.0.0-next.6+sha-7b5a86c';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
