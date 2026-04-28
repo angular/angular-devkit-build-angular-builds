c83a8ae55342b8b5ac399230bc819ebb481e22ff
Angular Builds <dev-infra@angular.dev>
2026-04-28 11:40:57 +0000

    49ae0ad fix(@angular-devkit/build-angular): upgrade postcss to 8.5.12

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.24+sha-bf387fb';
+const VERSION = '19.2.24+sha-49ae0ad';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
