c44ee7924e867be040d110f51195198db9c619e4
Angular Builds <dev-infra@angular.dev>
2026-04-29 15:12:56 +0000

    e6d80aa refactor(@angular/cli): remove find_examples tool and associated database rule

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.6+sha-dc1238e';
+const VERSION = '22.0.0-next.6+sha-e6d80aa';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
