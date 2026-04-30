4444cf33373f9f58ab4fb39816c880ad64c44f5c
Angular Builds <dev-infra@angular.dev>
2026-04-30 15:26:16 +0000

    870cc5d refactor: configure Gemini reviewer to ignore lockfile and changelog

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.7+sha-8aca55d';
+const VERSION = '22.0.0-next.7+sha-870cc5d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
