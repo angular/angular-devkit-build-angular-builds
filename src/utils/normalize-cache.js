4b19c1daced290aeac46df4004c8b1dc89ce68ef
Angular Builds <dev-infra@angular.dev>
2026-05-04 16:59:12 +0000

    7fb59ea fix(@schematics/angular): use service decorator in ng generate

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.7+sha-b2f7a03';
+const VERSION = '22.0.0-next.7+sha-7fb59ea';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
