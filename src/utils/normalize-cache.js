db251cd4b231721790e1b23708a632019cc50325
Angular Builds <dev-infra@angular.dev>
2026-05-07 15:48:40 +0000

    db3c5d8 refactor(@angular/cli): add validation and logging to npm manifest parsing

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-next.7+sha-93b5206';
+const VERSION = '22.0.0-next.7+sha-db3c5d8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
