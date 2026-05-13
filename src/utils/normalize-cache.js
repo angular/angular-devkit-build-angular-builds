77559df7ad05c67074951eb53dbee24f2609fa39
Angular Builds <dev-infra@angular.dev>
2026-05-13 09:22:26 +0000

    842fee0 fix(@angular/ssr): allow all hosts in common engine rendering options to prevent validation errors

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '19.2.25+sha-1aa23cd';
+const VERSION = '19.2.25+sha-842fee0';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
