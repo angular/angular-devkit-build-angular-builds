f6add7d41d64dce3fb20376ceda2caf24a626cfc
Angular Builds <dev-infra@angular.dev>
2026-05-15 13:20:34 +0000

    5dd4daf fix(@schematics/angular): support spy call arguments migration in refactor-jasmine-vitest

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0-next.0+sha-c150c08';
+const VERSION = '22.1.0-next.0+sha-5dd4daf';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
