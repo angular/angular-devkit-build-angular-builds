c9d40a0bfef8555d3f1d36f8d331bfbb12215ad1
Angular Builds <dev-infra@angular.dev>
2026-05-20 18:52:33 +0000

    feec5bf refactor(@angular/build): add missing `OutputFile` import

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.11+sha-6cc9349';
+const VERSION = '21.2.11+sha-feec5bf';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
