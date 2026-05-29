f1e1a2d69a75c6d849b0177a24f7a6583a0a29f8
Angular Builds <dev-infra@angular.dev>
2026-05-29 06:11:34 +0000

    21ca098 fix(@angular/ssr): support server-side rendering configuration options

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0-next.0+sha-b0e36d6';
+const VERSION = '22.1.0-next.0+sha-21ca098';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
