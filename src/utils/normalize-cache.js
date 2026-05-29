bc92e3e8107ae19f4d941f68c0b69ebaf0ec41e8
Angular Builds <dev-infra@angular.dev>
2026-05-29 13:34:36 +0000

    d46c082 fix(@angular/build): prevent esbuild service child process leakage

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.13+sha-aed4487';
+const VERSION = '21.2.13+sha-d46c082';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
