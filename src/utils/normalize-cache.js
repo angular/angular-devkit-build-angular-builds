edd7a6e2599f6ad1774da4c7d75eb14f7b285133
Angular Builds <dev-infra@angular.dev>
2026-06-01 07:30:27 +0000

    07c6c96 fix(@angular/ssr): add support for configuring trusted proxy headers via environment variable

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.26+sha-cae9e70';
+const VERSION = '20.3.26+sha-07c6c96';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
