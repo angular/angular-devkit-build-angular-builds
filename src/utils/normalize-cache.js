26c74a7c5fff9259d7437ee853668b4ce9c3b996
Angular Builds <dev-infra@angular.dev>
2026-06-03 13:14:33 +0000

    4d30ed2 build: update Angular framework and ng-packagr versions to 22.0.0

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.0-rc.3+sha-04b2353';
+const VERSION = '22.0.0-rc.3+sha-4d30ed2';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
