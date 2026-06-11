2d64367e06cd835e59829558849d646c06cc6a89
Angular Builds <dev-infra@angular.dev>
2026-06-11 13:58:56 +0000

    2fb9b3b test(@angular/cli): ignore engines in version-specifier E2E test when using yarn

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.27+sha-e3d5646';
+const VERSION = '20.3.27+sha-2fb9b3b';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
