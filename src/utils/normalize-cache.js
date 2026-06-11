e1357dd846c5190e3d93c6c559cda6e04fb7a736
Angular Builds <dev-infra@angular.dev>
2026-06-11 16:58:42 +0000

    d052e97 fix(@angular/ssr): prioritize options over environment variables in AngularNodeAppEngine

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.15+sha-d318ddd';
+const VERSION = '21.2.15+sha-d052e97';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
