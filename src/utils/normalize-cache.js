ff0e97ec110273a677c0f9da5bd6461d72357e0a
Angular Builds <dev-infra@angular.dev>
2026-06-16 11:42:08 +0000

    2879ed9 build: update bazel dependencies

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.1+sha-0f777a8';
+const VERSION = '22.0.1+sha-2879ed9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
