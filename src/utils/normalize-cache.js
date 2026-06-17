c5d1d8a35626bd4904da8021f0203b9546cd8111
Angular Builds <dev-infra@angular.dev>
2026-06-17 06:57:48 +0000

    5f7c032 fix(@angular/cli): update pacote to 21.5.1

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '20.3.28+sha-a75d78e';
+const VERSION = '20.3.28+sha-5f7c032';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
