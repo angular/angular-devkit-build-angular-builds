6791936bbbb704244615a524befec6f7c3b9e2f6
Angular Builds <dev-infra@angular.dev>
2026-06-29 13:38:06 +0000

    7c0d9f0 fix(@angular/build): load zone.js dynamically for library unit tests

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.4+sha-f75085f';
+const VERSION = '22.0.4+sha-7c0d9f0';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
