d43604ff26ea686586d948b1d0929c983b793ece
Angular Builds <dev-infra@angular.dev>
2026-07-07 16:49:15 +0000

    52ae7f8 feat(@angular/build): share persistent build cache across git worktrees

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0-next.2+sha-92da0d6';
+const VERSION = '22.1.0-next.2+sha-52ae7f8';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
