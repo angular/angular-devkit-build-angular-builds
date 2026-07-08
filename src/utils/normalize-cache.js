6fedd0641f03851e0190f4d2d4eb9016d80ca7f6
Angular Builds <dev-infra@angular.dev>
2026-07-08 14:39:17 +0000

    df49966 fix(@angular/build): fallback to build target preserveSymlinks option in karma runner

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.5+sha-c9aa354';
+const VERSION = '22.0.5+sha-df49966';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
