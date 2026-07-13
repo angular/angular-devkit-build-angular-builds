67b912f5f4d4c123b2f444a38d515455e135de31
Angular Builds <dev-infra@angular.dev>
2026-07-13 14:56:03 +0000

    b87cafe fix(@angular/cli): support resolving subproject dependencies in pnpm workspaces

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.6+sha-e85c10c';
+const VERSION = '22.0.6+sha-b87cafe';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
