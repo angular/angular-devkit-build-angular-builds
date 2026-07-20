aa18315b84620a822fd2eb09a1787172aa0bddb5
Angular Builds <dev-infra@angular.dev>
2026-07-20 15:46:31 +0000

    4187e5e fix(@angular/build): canonicalize drive letter casing for workspace root on Windows

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0-next.4+sha-dabd1c0';
+const VERSION = '22.1.0-next.4+sha-4187e5e';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
