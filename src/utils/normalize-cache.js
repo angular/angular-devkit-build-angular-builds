8e782dd559bf7c640d2084d6a10d99ba9b43642a
Angular Builds <dev-infra@angular.dev>
2026-07-23 14:10:29 +0000

    bc87c85 fix(@angular/cli): update MCP devserver tool names to conform to spec regex

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.0.7+sha-212a7bb';
+const VERSION = '22.0.7+sha-bc87c85';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
