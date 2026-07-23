76751486f52c3596172ba1bb1991fab56819ba66
Angular Builds <dev-infra@angular.dev>
2026-07-23 09:03:10 +0000

    0e3ca3d fix(@angular/build): support hyphens and underscores when stripping chunk hash in chunk optimizer

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.0-next.4+sha-5b5c873';
+const VERSION = '22.1.0-next.4+sha-0e3ca3d';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
