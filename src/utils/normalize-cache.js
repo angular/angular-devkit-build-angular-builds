1fa98dba86c906819c5029b1ce87a73f928f0017
Angular Builds <dev-infra@angular.dev>
2026-07-31 17:32:59 +0000

    72ae936 fix(@angular/cli): correctly report available package updates during bare ng update

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.2+sha-f5ed8fd';
+const VERSION = '22.1.2+sha-72ae936';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
