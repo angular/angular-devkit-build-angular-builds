0af0068bdbe16e8592b6accc0bddff8aa985eb03
Angular Builds <dev-infra@angular.dev>
2026-07-31 22:28:26 +0000

    1cdb3ae ci: use W3C-compliant Sauce Labs tunnelName and remove deprecated tunnelIdentifier

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.2+sha-6af906c';
+const VERSION = '22.1.2+sha-1cdb3ae';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
