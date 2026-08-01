cba50eac9c2f9aa7278f15218cb5ea1c26f2228a
Angular Builds <dev-infra@angular.dev>
2026-08-01 07:23:31 +0000

    10ba6d7 ci: fix Sauce Labs E2E browser tests with Sauce Connect 5 and legacy JWP capabilities

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.2+sha-1cdb3ae';
+const VERSION = '22.1.2+sha-10ba6d7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
