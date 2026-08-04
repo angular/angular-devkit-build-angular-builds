a6f2d6c645724a2a15bddb2836ad837c5aa9448c
Angular Builds <dev-infra@angular.dev>
2026-08-04 16:42:37 +0000

    25b7cf7 fix(@angular/build): support case-insensitive and alternative license file names

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.2+sha-212373a';
+const VERSION = '22.1.2+sha-25b7cf7';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
