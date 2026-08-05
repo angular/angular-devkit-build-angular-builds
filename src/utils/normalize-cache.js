b683cf3f9baede2c332be442f679bee9515b3dc1
Angular Builds <dev-infra@angular.dev>
2026-08-05 15:37:22 +0000

    3e9fed9 fix(@angular/build): set target for Rolldown dependency prebundling in Vite dev server

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.2.0-next.2+sha-04888ea';
+const VERSION = '22.2.0-next.2+sha-3e9fed9';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
