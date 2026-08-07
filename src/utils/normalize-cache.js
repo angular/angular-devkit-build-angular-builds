81a4ad0b3c07dde6c1821c6cf226e1dcac64cf28
Angular Builds <dev-infra@angular.dev>
2026-08-07 13:38:14 +0000

    956c657 fix(@angular/build): return only lowest version per target engine

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.3+sha-c86e932';
+const VERSION = '22.1.3+sha-956c657';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
