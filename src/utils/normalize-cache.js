abf1018fecd5b3b17b938a771906c372efacae4c
Angular Builds <dev-infra@angular.dev>
2026-08-14 13:52:49 +0000

    ff1d356 fix(@angular/build): preserve integrity and crossorigin in autoCsp loader

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.4+sha-95b4992';
+const VERSION = '22.1.4+sha-ff1d356';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
