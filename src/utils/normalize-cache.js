dac94b73c59a96518871a82d8384a8e2a712607f
Angular Builds <dev-infra@angular.dev>
2026-08-24 14:35:19 +0000

    b0047b0 fix(@angular-devkit/build-angular): update webpack-dev-server to 5.2.6

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '21.2.21+sha-90e8059';
+const VERSION = '21.2.21+sha-b0047b0';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
