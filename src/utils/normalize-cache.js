4d8c1a4ce79c8c74d68bbce353c0fd03f22606f4
Angular Builds <dev-infra@angular.dev>
2026-08-31 13:19:41 +0000

    5b850fd fix(@angular/build): fail build and exclude routes when prerendering fails

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.2.0-next.5+sha-ab6d7df';
+const VERSION = '22.2.0-next.5+sha-5b850fd';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
