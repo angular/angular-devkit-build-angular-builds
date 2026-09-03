b15e1cdbe209c37e8f5ed643477662c1b9b877de
Angular Builds <dev-infra@angular.dev>
2026-09-03 15:54:37 +0000

    54d712f fix(@angular/build): ensure parent directory exists in SQLite cache store

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.2.0-next.6+sha-55583c4';
+const VERSION = '22.2.0-next.6+sha-54d712f';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
