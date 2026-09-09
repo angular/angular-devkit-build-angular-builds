16a55e211a631f82215391bc9ac1bfc8ffe66928
Angular Builds <dev-infra@angular.dev>
2026-09-09 14:32:03 +0000

    87a1610 test(@angular/build): fix flakiness in dev-server rebuild error detection test

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.7+sha-7e133c4';
+const VERSION = '22.1.7+sha-87a1610';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
