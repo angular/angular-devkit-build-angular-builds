3ee45c4f5bd084e5be2ea2cf936b2b6ea6c86b9a
Angular Builds <dev-infra@angular.dev>
2026-09-16 07:20:36 +0000

    b0cb383 build: lock file maintenance

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.8+sha-f69e446';
+const VERSION = '22.1.8+sha-b0cb383';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
