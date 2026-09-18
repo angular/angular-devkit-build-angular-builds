d645e7cd08c88696801fb7887e382ddd96a5d3e5
Angular Builds <dev-infra@angular.dev>
2026-09-18 14:11:39 +0000

    b94dabd fix(@schematics/angular): update @types/node to a version vitest 5 accepts

--- src/utils/normalize-cache.js
+++ src/utils/normalize-cache.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.normalizeCacheOptions = normalizeCacheOptions;
 const node_path_1 = require("node:path");
 /** Version placeholder is replaced during the build process with actual package version */
-const VERSION = '22.1.8+sha-5aaad0a';
+const VERSION = '22.1.8+sha-b94dabd';
 function hasCacheMetadata(value) {
     return (!!value &&
         typeof value === 'object' &&
